f2023b1a8bfcb4e792ae818ab4ef779206fcb217
yonggyo1125 <yonggyo00@kakao.com>
2024-04-03 13:46:37 +0900

    .

--- 2. 실무 프로젝트(P-1)/exam01/src/components/Profile.js
+++ 2. 실무 프로젝트(P-1)/exam01/src/components/Profile.js
@@ -5,19 +5,19 @@ const Profile = () => {
     name: '이이름',
     age: 40,
   });
-
+  // const d1 = data;
   const changeProfile = () => {
-    //data.name = '김이름';
-    //data.age = 30;
-    //console.log(data);
-    //setData(data);
-    //setData({ name: '김이름', age: 30 });
+    // data.name = '김이름';
+    // data.age = 30;
+
+    //const d2 = data;
+    // const newData = { name: '김이름', age: 30 };
+    // console.log(data === newData);
+    //setData(newData);
     //setData({ ...data, name: '김이름' });
-
-    setData((state) => {
-      console.log('이전 상태값 : ', state);
-
-      return { ...state, name: '김이름' };
+    setData((prevState) => {
+      console.log('변경전 상태값 : ', prevState);
+      return { ...prevState, name: '김이름' };
     });
   };
 
