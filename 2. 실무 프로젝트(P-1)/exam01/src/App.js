a5dc87d29ad3ee8527a5aeefddcf9415affd1e39
yonggyo1125 <yonggyo00@kakao.com>
2024-04-03 10:30:42 +0900

    .

--- 2. 실무 프로젝트(P-1)/exam01/src/App.js
+++ 2. 실무 프로젝트(P-1)/exam01/src/App.js
@@ -1,11 +1,15 @@
-import MyComponent from './components/MyComponent2';
+//import MyComponent from './components/MyComponent2';
+import Counter from './components/Couter';
 
 const App = () => {
+  return <Counter />;
+  /*
   return (
     <MyComponent name={15}>
       <h1>반값습니다.</h1>
     </MyComponent>
   );
+  */
 };
 
 export default App;
--- 2. 실무 프로젝트(P-1)/exam01/src/components/Couter.js
+++ 2. 실무 프로젝트(P-1)/exam01/src/components/Couter.js
@@ -3,13 +3,16 @@ import { useState } from 'react';
 const Counter = () => {
   const [number, setNumber] = useState(0);
 
+  const plus = () => setNumber(number + 1);
+  const minus = () => setNumber(number - 1);
+
   return (
     <>
       <h1>{number}</h1>
-      <button type="button" onClick={setNumber(number + 1)}>
+      <button type="button" onClick={plus}>
         +1
       </button>
-      <button type="button" onClick={setNumber(number - 1)}>
+      <button type="button" onClick={minus}>
         -1
       </button>
     </>
