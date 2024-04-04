6e2b83860b33022cbe60b840a143b96e95422b6d
yonggyo1125 <yonggyo00@kakao.com>
2024-04-04 09:22:55 +0900

    .

---
+++ 2. 실무 프로젝트(P-1)/exam02/src/components/Color.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+
+class Color extends Component {
+  state = {
+    color: 'blue',
+  };
+
+  render() {
+    const { color } = this.state;
+    return (
+      <>
+        <div
+          style={{ background: color, width: '100px', height: '100px' }}
+        ></div>
+        <button type="button">RED</button>
+        <button type="button">ORANGE</button>
+        <button type="button">GREEN</button>
+      </>
+    );
+  }
+}
