d875aefc6da9c6a424f18b7e8da770d33a2db672
yonggyo1125 <yonggyo00@kakao.com>
2024-04-04 11:49:49 +0900

    .

--- 2. 실무 프로젝트(P-1)/exam02/src/components/JoinForm.js
+++ 2. 실무 프로젝트(P-1)/exam02/src/components/JoinForm.js
@@ -1,35 +1,36 @@
 import { useState } from 'react';
 
 const JoinForm = () => {
-  const [userId, setUserId] = useState('');
-  const [userPw, setUserPw] = useState('');
+  const [form, setForm] = useState({});
 
-  const onUserIdChange = (e) => setUserId(e.currentTarget.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <dl>
         <dt>아이디</dt>
         <dd>
-          <input type="text" onChange={onUserIdChange} />
+          <input type="text" name="userId" />
         </dd>
       </dl>
       <dl>
         <dt>비밀번호</dt>
         <dd>
-          <input type="password" />
+          <input type="password" name="userPw" />
         </dd>
       </dl>
       <dl>
         <dt>비밀번호 확인</dt>
         <dd>
-          <input type="password" />
+          <input type="password" name="confirmPw" />
         </dd>
       </dl>
       <dl>
         <dt>회원명</dt>
         <dd>
-          <input type="text" />
+          <input type="text" name="userNm" />
         </dd>
       </dl>
       <button type="submit">가입하기</button>
