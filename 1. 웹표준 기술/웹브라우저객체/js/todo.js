87351cf4406336052556a630a80e61dfd5020941
yonggyo1125 <yonggyo00@kakao.com>
2024-04-04 17:20:23 +0900

    .

---
+++ 1. 웹표준 기술/웹브라우저객체/js/todo.js
@@ -0,0 +1,23 @@
+const todo = {
+    /**
+     * 스케줄 추가 
+     * 
+     */
+    add() {
+
+    },
+    /**
+     * 스케줄 삭제
+     * 
+     */
+    remove() {
+
+    }
+};
+
+window.addEventListener("DOMContentLoaded", function () {
+  frmRegist.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+  });
+});
