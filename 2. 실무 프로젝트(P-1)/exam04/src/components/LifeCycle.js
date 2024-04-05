2d41f289f54f82f7abd689a0ea683c1d080bcd47
yonggyo1125 <yonggyo00@kakao.com>
2024-04-05 12:50:44 +0900

    .

--- 2. 실무 프로젝트(P-1)/exam04/src/components/LifeCycle.js
+++ 2. 실무 프로젝트(P-1)/exam04/src/components/LifeCycle.js
@@ -1,8 +1,46 @@
 import { Component } from 'react';
 
 class LifeCycle extends Component {
+  state = {
+    number: 0,
+  };
+
+  constructor(props) {
+    super(props);
+
+    console.log('constructor');
+  }
+
+  static getDerivedStateFromProps(prevProps, prevState) {
+    console.log('getDerivedStateFromProps');
+
+    return null;
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
   render() {
-    return <h1>출력!</h1>;
+    console.log('render');
+    const { number } = this.state;
+    return (
+      <>
+        <h1>{number}</h1>
+        <button
+          type="button"
+          onClick={() => this.setState({ number: number + 1 })}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          onClick={() => this.setState({ number: number - 1 })}
+        >
+          -1
+        </button>
+      </>
+    );
   }
 }
 
