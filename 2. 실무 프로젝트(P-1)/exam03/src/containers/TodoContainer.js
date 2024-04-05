1ba22fcc7c2850fb05da857d9f109af24194d033
yonggyo1125 <yonggyo00@kakao.com>
2024-04-05 17:50:28 +0900

    .

--- 2. 실무 프로젝트(P-1)/exam03/src/containers/TodoContainer.js
+++ 2. 실무 프로젝트(P-1)/exam03/src/containers/TodoContainer.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import AddTodo from '../components/AddTodo';
 import TodoList from '../components/TodoList';
 
@@ -11,6 +11,7 @@ const intialValue = [
 let submitFunc;
 
 const TodoContainer = () => {
+  // 업데이트 시, 매번 호출
   const [items, setItems] = useState(intialValue);
   const [todo, setTodo] = useState('');
   const [message, setMessage] = useState('');
@@ -18,47 +19,63 @@ const TodoContainer = () => {
   let id = useRef(4); // 할일 id
 
   // 할일 등록 처리
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
 
-    if (!todo.trim()) {
-      setMessage('할일을 입력하세요.');
-      return;
-    }
+      if (!todo.trim()) {
+        setMessage('할일을 입력하세요.');
+        return;
+      }
 
-    const newItems = items.concat({
-      id: id.current,
-      title: todo.trim(),
-      done: false,
-    });
+      setItems((prevItems) => {
+        return prevItems.concat({
+          id: id.current,
+          title: todo.trim(),
+          done: false,
+        });
+      });
 
-    setItems(newItems);
+      id.current++;
 
-    id.current++;
-
-    setTodo('');
-    setMessage('');
-  };
+      setTodo('');
+      setMessage('');
+    },
+    [todo],
+  );
 
   console.log('같은 함수 : ? ', submitFunc === onSubmit);
   submitFunc = onSubmit;
 
   // 할일 입력시 todo 상태값 변경
-  const onChange = (e) => setTodo(e.currentTarget.value);
+  const onChange = useCallback((e) => setTodo(e.currentTarget.value), []);
 
   // 할일 목록 완료 여부 토글(true -> false, false -> true)
-  const onToggle = (id) => {
-    const newItems = items.map((item) =>
-      item.id === id ? { ...item, done: !item.done } : item,
-    );
-    setItems(newItems);
-  };
+  const onToggle = useCallback((id) => {
+    /*
+      const newItems = items.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item,
+      );
+      setItems(newItems);
+    */
+
+    setItems((prevItems) => {
+      return prevItems.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item,
+      );
+    });
+  }, []);
 
   // 할일 목록 제거
-  const onRemove = (id) => {
-    const newItems = items.filter((item) => item.id !== id);
-    setItems(newItems);
-  };
+  const onRemove = useCallback((id) => {
+    /*
+      const newItems = items.filter((item) => item.id !== id);
+      setItems(newItems);
+      */
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id !== id);
+    });
+  }, []);
 
   return (
     <>
