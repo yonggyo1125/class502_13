1f45da9ca8f1aad02eacdad2fc9428dc1e85f2cd
yonggyo1125 <yonggyo00@kakao.com>
2024-04-08 14:48:39 +0900

    .

---
+++ 2. 실무 프로젝트(P-1)/exam05/src/components/SassComponent.js
@@ -0,0 +1,15 @@
+const SassComponent = () => {
+  return (
+    <div className="SassComponent">
+      <div className="box red"></div>
+      <div className="box orange"></div>
+      <div className="box yellow"></div>
+      <div className="box green"></div>
+      <div className="box blue"></div>
+      <div className="box indigo"></div>
+      <div className="box violet"></div>
+    </div>
+  );
+};
+
+export default SassComponent;
