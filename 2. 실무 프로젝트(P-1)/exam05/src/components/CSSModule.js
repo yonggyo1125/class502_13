d4cc1d44b107602b60e631f96fd0bb5bd11fd4a2
yonggyo1125 <yonggyo00@kakao.com>
2024-04-08 16:34:35 +0900

    .

--- 2. 실무 프로젝트(P-1)/exam05/src/components/CSSModule.js
+++ 2. 실무 프로젝트(P-1)/exam05/src/components/CSSModule.js
@@ -1,12 +1,21 @@
 import styles from './CSSModule.module.scss';
-console.log(styles);
+import { useState } from 'react';
+import classNames from 'classnames';
 
 const CSSModule = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <div className={styles.wrapper}>
-      <span className="commonColor">안녕하세요</span>,
-      <span className={styles.highlight}>반갑습니다.</span>
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        <span className="commonColor">안녕하세요</span>,
+        <span className={styles.highlight}>반갑습니다.</span>
+      </div>
+      <div className={classNames('menus', ['cls1', 'cls2'], { on: visible })}>메뉴</div>
+      <button type="button" onClick={() => setVisible(!visible)}>
+        클릭
+      </button>
+    </>
   );
 };
 
