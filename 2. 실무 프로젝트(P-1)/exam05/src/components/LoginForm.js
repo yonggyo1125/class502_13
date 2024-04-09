b4c40647559b9e1e58e91a18038dea7e0d899c6a
yonggyo1125 <yonggyo00@kakao.com>
2024-04-09 10:00:48 +0900

    .

--- 2. 실무 프로젝트(P-1)/exam05/src/components/LoginForm.js
+++ 2. 실무 프로젝트(P-1)/exam05/src/components/LoginForm.js
@@ -1,12 +1,31 @@
+import styled from 'styled-components';
+
+const FormBox = styled.form`
+  width: 420px;
+  margin: 0 auto;
+`;
+
+const InputBox = styled.input`
+  display: block;
+  height: 45px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 100%;
+
+  & + & {
+    margin-top: 5px;
+  }
+`;
+
 const LoginForm = () => {
   return (
     <>
       <h1>로그인</h1>
-      <form autoComplete="off">
-        <input type="text" placeholder="아이디" />
-        <input type="password" placeholder="비밀번호" />
+      <FormBox autoComplete="off">
+        <InputBox type="text" placeholder="아이디" />
+        <InputBox type="password" placeholder="비밀번호" />
         <button type="submit">로그인</button>
-      </form>
+      </FormBox>
     </>
   );
 };
