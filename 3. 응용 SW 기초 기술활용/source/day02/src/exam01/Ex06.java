13c732760bf582a47d296e37c1f672250ac2dfe0
yonggyo1125 <yonggyo00@kakao.com>
2024-04-15 11:00:40 +0900

    수업 소스

---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/Ex06.java
@@ -0,0 +1,22 @@
+package exam01;
+
+public class Ex06 {
+    public static void main(String[] args) {
+        int total = 0;
+        int i = 0, j = 10;
+        for ( ; ; i++, j += 2) {
+            if (i > 100) {
+                break;
+            }
+
+            total += i;
+            System.out.println("j:" + j);
+
+        }
+
+        System.out.println("합계: " + total);
+    }
+
+    // for ( ; ; ) { } == while(true) { ... }
+
+}
---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/Ex07.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class Ex07 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 9; i++) {
+            System.out.println(i + "단 ============");
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " X " + j + " = " + (i * j));
+            }
+            System.out.println(); // \n
+        }
+    }
+}
