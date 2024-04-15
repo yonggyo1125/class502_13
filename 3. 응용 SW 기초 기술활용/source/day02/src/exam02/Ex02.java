932bcf2f2162a1b151c2f3111607365a853a5e29
yonggyo1125 <yonggyo00@kakao.com>
2024-04-15 12:51:17 +0900

    수업 소스

---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam02/Ex02.java
@@ -0,0 +1,15 @@
+package exam02;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int[] nums = new int[4]; // int형 변수를 4개 생성, 순차자료구조 구현
+        // nums[0], nums[1], nums[2], nums[3]
+        nums[0] = 10;
+        nums[1] = 20;
+        nums[2] = 30;
+
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+    }
+}
