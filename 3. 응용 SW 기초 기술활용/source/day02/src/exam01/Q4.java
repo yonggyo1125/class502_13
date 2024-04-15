30ddaa520ae65d075f9fbb5a1bdd92a1b0a7f809
yonggyo1125 <yonggyo00@kakao.com>
2024-04-15 12:01:51 +0900

    수업 소스

--- 3. 응용 SW 기초 기술활용/source/day02/src/exam01/Q4.java
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/Q4.java
@@ -1,14 +1,18 @@
 package exam01;
 
-public class Ex07 {
+public class Q4 {
     public static void main(String[] args) {
+        // 구구단을 홀수 단만 출력하도록 프로그램을 만드시오.
         for (int i = 2; i <= 9; i++) {
-            System.out.println(i + "단 ============");
+
+            if (i % 2 == 0) {
+                continue;
+            }
+
+            System.out.println(i + "단 ===========");
             for (int j = 1; j <= 9; j++) {
-                //System.out.println(i + " X " + j + " = " + (i * j));
                 System.out.printf("%d X %d = %d%n", i, j, i * j);
             }
-            System.out.println(); // \n
         }
     }
 }
---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/Q5.java
@@ -0,0 +1,7 @@
+package exam01;
+
+public class Q5 {
+    public static void main(String[] args) {
+
+    }
+}
