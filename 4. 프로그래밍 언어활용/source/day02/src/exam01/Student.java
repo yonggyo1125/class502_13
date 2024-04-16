90a044acc01f89c3c393458b1a22624e557da060
yonggyo1125 <yonggyo00@kakao.com>
2024-04-16 09:12:04 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어활용/source/day02/src/exam01/Student.java
@@ -0,0 +1,11 @@
+package exam01;
+
+public class Student {
+    int id; // 학번
+    String name; // 이름
+    String subject; // 과목
+
+    void study() {
+        System.out.printf("%s가 %s를 공부한다.%n", name, subject);
+    }
+}
