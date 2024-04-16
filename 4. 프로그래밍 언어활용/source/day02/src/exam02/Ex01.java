abb6614465ec962152192ab38e1695a45c621ca7
yonggyo1125 <yonggyo00@kakao.com>
2024-04-16 16:26:38 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day02/src/exam02/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day02/src/exam02/Ex01.java
@@ -3,9 +3,11 @@ package exam02;
 public class Ex01 { // exam02.Ex01
     public static void main(String[] args) {
        Schedule s1 = new Schedule();
-        //s1.year = 2024;
-        //s1.month = 2;
-        //s1.day = 31; // 지양, 통제가 불가 하므로
+        /*
+        s1.year = 2024;
+        s1.month = 2;
+        s1.day = 31; // 지양, 통제가 불가 하므로
+        */
 
         s1.setYear(2024);
         s1.setMonth(2);
