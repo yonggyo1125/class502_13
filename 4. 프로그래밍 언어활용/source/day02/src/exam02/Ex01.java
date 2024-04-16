aae58dde28f71d6b255d71f50532bf18a48dfbe0
yonggyo1125 <yonggyo00@kakao.com>
2024-04-16 16:01:48 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day02/src/exam02/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day02/src/exam02/Ex01.java
@@ -11,6 +11,8 @@ public class Ex01 { // exam02.Ex01
         s1.setMonth(2);
         s1.setDay(31);
 
+        int month = s1.getMonth();
+        System.out.println(month);
 
         s1.showDate();
     }
