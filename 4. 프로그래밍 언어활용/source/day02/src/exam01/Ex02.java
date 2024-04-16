16ca8162e289507fd89d832ea652fa95ebae0521
yonggyo1125 <yonggyo00@kakao.com>
2024-04-16 10:00:56 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day02/src/exam01/Ex02.java
+++ 4. 프로그래밍 언어활용/source/day02/src/exam01/Ex02.java
@@ -2,11 +2,13 @@ package exam01;
 
 public class Ex02 {
     public static void main(String[] args) {
-        int r = add(10, 20);
-        System.out.println(r);
+        int n1 = 10;
+        int n2 = 20;
+        int r = add(n1, n2);
+        //System.out.println(r);
     }
 
-    static int add(int n1, int n2) {
+    static int add(int n1, int n2) {  // 함수 지역
         int result = n1 + n2;
 
         return result;
