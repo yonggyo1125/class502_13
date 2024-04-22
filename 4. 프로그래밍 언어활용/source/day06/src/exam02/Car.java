5067e1aa400343f844d44b5b88dd1fde6a8b46cf
yonggyo1125 <yonggyo00@kakao.com>
2024-04-22 10:01:08 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day06/src/exam02/Car.java
+++ 4. 프로그래밍 언어활용/source/day06/src/exam02/Car.java
@@ -5,7 +5,7 @@ public abstract class Car {
     public abstract void press();
     public abstract void turnoff();
 
-    public void run() { // 템플릿 메서드 패턴
+    public final void run() { // 템플릿 메서드 패턴
         start();
         press();
         turnoff();
---
+++ 4. 프로그래밍 언어활용/source/day06/src/exam02/Ex01.java
@@ -0,0 +1,8 @@
+package exam02;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        SportCar sportCar = new SportCar();
+        sportCar.run();
+    }
+}
