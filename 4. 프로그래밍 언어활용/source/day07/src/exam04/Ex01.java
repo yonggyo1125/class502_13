8d5aa224f35620e7065b21f2baac595f200c9093
yonggyo1125 <yonggyo00@kakao.com>
2024-04-23 15:00:59 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어활용/source/day07/src/exam04/Ex01.java
@@ -0,0 +1,8 @@
+package exam04;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        LoginService service = new LoginService();
+        service.login("user02", "123456");
+    }
+}
