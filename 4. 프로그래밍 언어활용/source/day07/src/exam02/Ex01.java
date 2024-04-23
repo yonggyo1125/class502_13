b56bc24aa278d4b047206acbf2c68038b68b3e94
yonggyo1125 <yonggyo00@kakao.com>
2024-04-23 10:01:17 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어활용/source/day07/src/exam02/Ex01.java
@@ -0,0 +1,13 @@
+package exam02;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 0;
+        int result = num1 / num2;
+
+        System.out.println(result);
+
+        System.out.println("매우 중요한 실행코드...");
+    }
+}
