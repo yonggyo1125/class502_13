a549424f03a93916908581d692d121e2a5b87c25
yonggyo1125 <yonggyo00@kakao.com>
2024-04-23 11:01:34 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day07/src/exam02/Ex02.java
+++ 4. 프로그래밍 언어활용/source/day07/src/exam02/Ex02.java
@@ -7,10 +7,16 @@ public class Ex02 {
     public static void main(String[] args) {
         // throw new FileNotFoundException(...)
         try {
-            FileInputStream fis = new FileInputStream("a.txt");
+            FileInputStream fis = new FileInputStream("b.txt");
+
+            System.out.println("파일 처리...");
 
         } catch (FileNotFoundException e) {
             System.out.println("예외 발생!");
+            String message = e.getMessage();
+            System.out.println(message);
         }
+
+        System.out.println("매우 중요한 실행 코드....");
     }
 }
