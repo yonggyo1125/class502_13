a6c164c2c78f6ddc90b984409848f22d0d238adf
yonggyo1125 <yonggyo00@kakao.com>
2024-04-24 12:39:23 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어활용/source/day08/src/exam03/Ex01.java
@@ -0,0 +1,12 @@
+package exam03;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Integer num1 = new Integer(10);
+        double num2 = num1.doubleValue();
+
+        String str = "1000";
+        int num3 = Integer.parseInt(str);
+
+    }
+}
