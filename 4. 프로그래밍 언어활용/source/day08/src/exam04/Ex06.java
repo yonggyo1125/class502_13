28d94a434903b7b07a442dcbd2ab3f41f55e2a65
yonggyo1125 <yonggyo00@kakao.com>
2024-04-24 17:00:32 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day08/src/exam04/Ex06.java
+++ 4. 프로그래밍 언어활용/source/day08/src/exam04/Ex06.java
@@ -1,7 +1,12 @@
 package exam04;
 
+import java.util.Arrays;
+
 public class Ex06 {
     public static void main(String[] args) {
-
+        int[][] nums1 = {{10, 20, 30}, {40,50,60}};
+        int[][] nums2 = {{10, 20, 30}, {40, 55, 60}};
+        boolean isSame = Arrays.deepEquals(nums1, nums2);
+        System.out.println(isSame);
     }
 }
