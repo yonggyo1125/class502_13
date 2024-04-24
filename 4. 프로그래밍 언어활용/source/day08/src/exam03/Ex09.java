7d139ed8ea72c04160ec519d79bfd8d0c120be71
yonggyo1125 <yonggyo00@kakao.com>
2024-04-24 16:01:37 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어활용/source/day08/src/exam03/Ex09.java
@@ -0,0 +1,7 @@
+package exam03;
+
+public class Ex09 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class cls = Class.forName("exam03.Person");
+    }
+}
---
+++ 4. 프로그래밍 언어활용/source/day08/src/exam04/Ex01.java
@@ -0,0 +1,15 @@
+package exam04;
+
+import static java.lang.Math.*;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        System.out.println(abs(-10)); // 10
+        System.out.println(ceil(10.2)); // 11 // 올림
+        System.out.println(floor(10.2)); // 10 // 버림
+        System.out.println(round(10.5)); // 11 // 반올림
+        System.out.println(pow(2,4)); // 2의 4승
+        System.out.println(sqrt(9)); // 3
+
+    }
+}
