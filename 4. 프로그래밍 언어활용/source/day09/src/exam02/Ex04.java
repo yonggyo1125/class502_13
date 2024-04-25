11634eb6a59beca4a5639c750b6c84aafeb80794
yonggyo1125 <yonggyo00@kakao.com>
2024-04-25 11:00:32 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day09/src/exam02/Ex04.java
+++ 4. 프로그래밍 언어활용/source/day09/src/exam02/Ex04.java
@@ -10,7 +10,9 @@ public class Ex04 {
         System.out.print("문장을 입력하세요:");
         String str = sc.nextLine();
 
-        Pattern p1 = Pattern.compile("[a-z]");
+        //Pattern p1 = Pattern.compile("[a-zA-Z]"); // 대소문자 구분없이 모든 알파벳
+        // 알파벳 대소문자 구분 없이 체크
+        Pattern p1 = Pattern.compile("[a-z]", Pattern.CASE_INSENSITIVE);
         Matcher m1 = p1.matcher(str);
         if (!m1.find()) {
             System.out.println("알파벳이 포함되지 않았습니다.");
