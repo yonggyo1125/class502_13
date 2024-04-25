34da1b57e63a852baa43fd18ac25695e756a4375
yonggyo1125 <yonggyo00@kakao.com>
2024-04-25 14:44:23 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어활용/source/day09/src/exam02/Ex13.java
@@ -0,0 +1,15 @@
+package exam02;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Ex13 {
+    public static void main(String[] args) {
+        String str = "이이름: 010-0000-0000\n김이름 010-1111-1111";
+        Pattern p1 = Pattern.compile("([^:]+):\\s*(01[016]\\D*\\d{3,4}\\D*\\d{4})", Pattern.MULTILINE);
+        Matcher m1 = p1.matcher(str);
+        while(m1.find()) {
+            System.out.printf("이름: %s, 전화번호: %s%n", m1.group(1), m1.group(2));
+        }
+    }
+}
