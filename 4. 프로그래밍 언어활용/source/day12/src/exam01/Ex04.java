47d4e41b3813c26a6e700dce7815ac1992e802ed
yonggyo1125 <yonggyo00@kakao.com>
2024-04-30 10:19:26 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day12/src/exam01/Ex04.java
+++ 4. 프로그래밍 언어활용/source/day12/src/exam01/Ex04.java
@@ -5,6 +5,13 @@ import java.util.HashSet;
 public class Ex04 {
     public static void main(String[] args) {
         HashSet<String> names = new HashSet<>();
+        names.add("이름2");
+        names.add("이름2");
+        names.add("이름1");
+        names.add("이름3");
+        names.add("이름5");
+        names.add("이름4");
 
+        System.out.println(names); // names.toString()
     }
 }
