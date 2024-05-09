a9df4cd5fe48b08c4a6adc67b1a610750bd8affb
yonggyo1125 <yonggyo00@kakao.com>
2024-05-09 15:40:39 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day18/src/exam02/CalendarTest.java
+++ 4. 프로그래밍 언어활용/source/day18/src/exam02/CalendarTest.java
@@ -21,15 +21,22 @@ public class CalendarTest {
                 int month = Integer.parseInt(data[1]);
 
                 System.out.printf("---- %d년 %d월 ----%n", year, month);
+                System.out.println("일  월  화  수  목  금  토");
 
 
             } catch (Exception e) { // 숫자 형식 오류가 발생한 경우
                 System.out.println("입력 형식이 잘못되었습니다. 입력 예) 2024 5");
-                continue;
             }
 
         }
+    }
 
+    public static Object getCalendar(int year, int month) {
+        /**
+         * 1. 매월의 1일이 어떤 요일에 시작하는지
+         * 2. 매월의 종료일이 무엇인지(30, 31, 28, 29)
+         */
 
+        return null;
     }
 }
