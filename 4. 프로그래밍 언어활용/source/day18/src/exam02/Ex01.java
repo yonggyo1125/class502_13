900f9ad4ec0c0f133abd31b8ca69d0864a388c57
yonggyo1125 <yonggyo00@kakao.com>
2024-05-09 15:46:58 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어활용/source/day18/src/exam02/Ex01.java
@@ -0,0 +1,17 @@
+package exam02;
+
+import java.time.LocalDate;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        LocalDate startDate = LocalDate.of(today.getYear(), today.getMonth(), 1);
+
+        int yoil = startDate.getDayOfWeek().getValue(); // 1(월)~7(일)
+        System.out.println(yoil);
+
+        LocalDate endDate = startDate.plusMonths(1).minusDays(1);
+        System.out.println(endDate);
+
+    }
+}
