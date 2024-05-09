085baf8df4bad42ba2dd1af777607002611f03ea
yonggyo1125 <yonggyo00@kakao.com>
2024-05-10 07:27:36 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day18/src/exam02/CalendarTest.java
+++ 4. 프로그래밍 언어활용/source/day18/src/exam02/CalendarTest.java
@@ -1,6 +1,9 @@
 package exam02;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class CalendarTest {
     public static void main(String[] args) {
@@ -21,9 +24,22 @@ public class CalendarTest {
                 int month = Integer.parseInt(data[1]);
 
                 System.out.printf("---- %d년 %d월 ----%n", year, month);
-                System.out.println("일  월  화  수  목  금  토");
+                System.out.println(" S   M   T   W   T   F   S");
+                // 달력 데이터 조회
+                int[] days = getCalendar(year, month);
+                for (int i = 0; i < days.length; i++) {
+                    int day = days[i];
+                    if (day > 0) {
+                        System.out.printf("%02d  ", day);
+                    } else {
+                        System.out.print("    ");
+                    }
 
-
+                    if ((i+1) % 7 == 0) {
+                        System.out.println();
+                    }
+                }
+                System.out.println();
             } catch (Exception e) { // 숫자 형식 오류가 발생한 경우
                 System.out.println("입력 형식이 잘못되었습니다. 입력 예) 2024 5");
             }
@@ -31,12 +47,16 @@ public class CalendarTest {
         }
     }
 
-    public static Object getCalendar(int year, int month) {
+    public static int[] getCalendar(int year, int month) {
         /**
          * 1. 매월의 1일이 어떤 요일에 시작하는지
          * 2. 매월의 종료일이 무엇인지(30, 31, 28, 29)
          */
+        LocalDate startDate = LocalDate.of(year, month, 1);
+        LocalDate endDate = startDate.plusMonths(1).minusDays(1);
 
-        return null;
+        int start = startDate.getDayOfWeek().getValue() % 7 * -1 + 1;
+
+        return IntStream.rangeClosed(start, endDate.getDayOfMonth()).toArray();
     }
 }
