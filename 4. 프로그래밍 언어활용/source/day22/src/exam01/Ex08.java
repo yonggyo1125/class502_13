7049624e2ba1346ad79bf85687ebe271cd2f7c22
yonggyo1125 <yonggyo00@kakao.com>
2024-05-16 15:40:40 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex08.java
+++ 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex08.java
@@ -1,4 +1,20 @@
 package exam01;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Ex08 {
+    public static void main(String[] args) {
+        try (FileOutputStream fos = new FileOutputStream("book.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            Book book1 = new Book(1000, "책1", "저자1");
+
+            oos.writeObject(book1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
