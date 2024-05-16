bd33741896a8c2809b0eb9fc04d1a3ef146b8dfb
yonggyo1125 <yonggyo00@kakao.com>
2024-05-16 16:44:27 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex11.java
@@ -0,0 +1,26 @@
+package exam01;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.List;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("data.obj");
+            ObjectInputStream ois = new ObjectInputStream(fis)) {
+
+            HashMap<String, Object> data = (HashMap<String, Object>)ois.readObject();
+
+            List<Book> books = (List<Book>)data.get("books");
+            String str = (String)data.get("str");
+
+            books.forEach(System.out::println);
+            System.out.println(str);
+
+        } catch(IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
