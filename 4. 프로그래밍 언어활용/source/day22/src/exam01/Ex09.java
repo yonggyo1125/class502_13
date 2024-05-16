bd9e11ee2e5f736eb21ce060cf0d6c0f120fcb2d
yonggyo1125 <yonggyo00@kakao.com>
2024-05-16 16:35:08 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex09.java
+++ 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex09.java
@@ -13,14 +13,17 @@ public class Ex09 {
              * Book book = new Book(..., ..., ...);
              * - 복구시에 필요한 데이터가 저장 -> 다시 객체 생성, 값 대입
              */
+
+            String str = (String)ois.readObject();
+            System.out.println(str);
+
             Book book1 = (Book)ois.readObject();
             System.out.println(book1);
 
             Book book2 = (Book)ois.readObject();
             System.out.println(book2);
 
-            String str = (String)ois.readObject();
-            System.out.println(str);
+
 
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
---
+++ 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex10.java
@@ -0,0 +1,34 @@
+package exam01;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        try (FileOutputStream fos = new FileOutputStream("data.obj");
+            ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+
+            HashMap<String, Object> data = new HashMap<>();
+
+            Book book1 = new Book(1000, "책1", "저자1");
+            Book book2 = new Book(1001, "책2", "저자2");
+
+            List<Book> books = Arrays.asList(
+              new Book(1002, "책3", "저자3"),
+              new Book(1003, "책4", "저자4")
+            );
+
+            data.put("book1", book1);
+            data.put("book2", book2);
+            data.put("books", books);
+            data.put("str", "안녕하세요.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
