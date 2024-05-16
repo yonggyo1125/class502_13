32fbe03f5060d777f10660fc919ecf17f8c3e067
yonggyo1125 <yonggyo00@kakao.com>
2024-05-16 15:20:18 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex06.java
+++ 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex06.java
@@ -4,7 +4,13 @@ import java.io.File;
 import java.io.IOException;
 
 public class Ex06 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
         File tmpFile = File.createTempFile("tmp", ".log", new File("D:/fstudy"));
+        //tmpFile.delete(); // 바로 삭제
+        tmpFile.deleteOnExit();  // 애플리케이션이 종료되면 삭제
+
+        Thread.sleep(5000);
+        System.out.println("종료!");
     }
 }
