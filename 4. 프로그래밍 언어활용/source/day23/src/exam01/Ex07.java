18e43d7fbbb7669754ede2e88808c5f6224210c8
yonggyo1125 <yonggyo00@kakao.com>
2024-05-17 16:46:21 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day23/src/exam01/Ex07.java
+++ 4. 프로그래밍 언어활용/source/day23/src/exam01/Ex07.java
@@ -3,7 +3,7 @@ package exam01;
 public class Ex07 {
     private static boolean autoSave = false;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread th = new Thread(() -> {
             while (true) {
                 try {
@@ -16,5 +16,13 @@ public class Ex07 {
         });
 
 
+        for (int i = 1; i <= 10; i++) {
+
+            Thread.sleep(1000);
+            System.out.println(i);
+            if (i == 3) {
+                autoSave = true;
+            }
+        }
     }
 }
