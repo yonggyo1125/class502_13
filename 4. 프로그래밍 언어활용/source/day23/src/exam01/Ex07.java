2c7165c61cadaa995b64140ae36c0cd40a1fbc03
yonggyo1125 <yonggyo00@kakao.com>
2024-05-17 17:28:01 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day23/src/exam01/Ex07.java
+++ 4. 프로그래밍 언어활용/source/day23/src/exam01/Ex07.java
@@ -4,6 +4,7 @@ public class Ex07 {
     private static boolean autoSave = false;
 
     public static void main(String[] args) throws InterruptedException {
+        /*
         Thread th = new Thread(() -> {
             while (true) {
                 try {
@@ -17,7 +18,9 @@ public class Ex07 {
 
         th.setDaemon(true); // 현재 작업 중인 쓰레드가 종료 -> 함께 종료
         th.start();
-
+        */
+        AutoSave th = new AutoSave();
+        th.start();
 
         for (int i = 1; i <= 10; i++) {
 
@@ -27,5 +30,31 @@ public class Ex07 {
                 autoSave = true;
             }
         }
+    }
+}
+
+class AutoSave implements Runnable {
+
+    private Thread th;
+
+    public AutoSave() {
+        th = new Thread(this);
+        th.setDaemon(true);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+            }
+
+            System.out.println("저장!");
+        }
+    }
+
+    public void start() {
+        th.start();
     }
 }
