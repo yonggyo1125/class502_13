d9620792a79a8cc719e2fe7de6a3160c776c7087
yonggyo1125 <yonggyo00@kakao.com>
2024-05-20 16:59:17 +0900

    수업 소스

--- 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex08.java
+++ 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex08.java
@@ -1,7 +1,29 @@
 package exam01;
 
 public class Ex08 {
+    public static void main(String[] args) throws InterruptedException {
+        Ex08_1 th1 = new Ex08_1("*");
+        Ex08_1 th2 = new Ex08_1("**");
+        Ex08_1 th3 = new Ex08_1("***");
 
+        th1.start();
+        th2.start();
+        th3.start();
+
+        Thread.sleep(2000);
+        th1.suspend(); // 일시 정지
+
+        Thread.sleep(2000);
+        th2.suspend(); // 일시 정지
+
+        Thread.sleep(3000);
+        th1.stop(); // 정지(종료)
+        th2.stop(); // 정지(종료)
+
+        Thread.sleep(2000);
+        th3.stop(); // 정지(종료)
+
+    }
 }
 
 class Ex08_1 implements Runnable {
@@ -17,7 +39,14 @@ class Ex08_1 implements Runnable {
 
     @Override
     public void run() {
-
+        while(!stopped) {
+            if (!suspended) {
+                System.out.println(th.getName());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+            }
+        }
     }
 
     public void start() {
