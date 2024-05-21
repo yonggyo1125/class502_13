199552966060e925f2298471225e8016630a262d
yonggyo1125 <yonggyo00@kakao.com>
2024-05-21 14:29:31 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어활용/source/day24/src/exam02/Ex01.java
@@ -0,0 +1,23 @@
+package exam02;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Ex01_1 ex01_1 = new Ex01_1();
+        Thread th1 = new Thread(ex01_1);
+        Thread th2 = new Thread(ex01_1);
+    }
+}
+
+class Ex01_1 implements Runnable {
+
+    private Account acc = new Account();
+
+    @Override
+    public void run() {
+        while(acc.getBalance() > 0) {
+            int money = (int)(Math.random() * 3 + 1) * 100; // 100~300
+            acc.withdraw(money);
+            System.out.println("balance : " + acc.getBalance());
+        }
+    }
+}
