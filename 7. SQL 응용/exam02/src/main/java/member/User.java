99b739a95e173d9601a35b9e64cace72b0b7b938
yonggyo1125 <yonggyo00@kakao.com>
2024-05-22 09:41:33 +0900

    수업 소스

--- 7. SQL 응용/exam02/src/main/java/member/User.java
+++ 7. SQL 응용/exam02/src/main/java/member/User.java
@@ -1,14 +1,38 @@
 package member;
 
+import java.time.LocalDateTime;
+
 public class User {
 
     private String userId;
     private String userNm;
+    private String email;
+    private LocalDateTime regDt;
 
     private User() {}
 
 
     static class Builder {
         private User user = new User();
+
+        public Builder userId(String userId) {
+            user.userId = userId;
+
+            return this;
+        }
+
+        public Builder userNm(String userNm) {
+            user.userNm = userNm;
+
+            return this;
+        }
+
+        public Builder email(String email) {
+            user.email = email;
+
+            return this;
+        }
+
+
     }
 }
