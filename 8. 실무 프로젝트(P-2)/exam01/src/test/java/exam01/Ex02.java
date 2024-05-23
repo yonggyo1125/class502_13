5f18841a397e8bacc765e688272a55b471db1d2d
yonggyo1125 <yonggyo00@kakao.com>
2024-05-23 12:25:01 +0900

    수업 소스

--- 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex02.java
+++ 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex02.java
@@ -3,10 +3,8 @@ package exam01;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDateTime;
 
 public class Ex02 {
     private String url = "jdbc:oracle:thin:@localhost:1521:XE";
@@ -31,7 +29,35 @@ public class Ex02 {
 
             pstmt.setString(1, "%" + keyword + "%");
 
+            ResultSet rs = pstmt.executeQuery();
 
+            while(rs.next()) {
+                long userNo = rs.getLong("USER_NO");
+                String userId = rs.getString("USER_ID");
+                String userPw = rs.getString("USER_PW");
+                String userNm = rs.getString("USER_NM");
+                String mobile = rs.getString("MOBILE");
+                LocalDateTime regDt = rs.getTimestamp("REG_DT").toLocalDateTime();
+
+                System.out.printf("USER_NO:%d, USER_ID:%s, USER_PW:%s, USER_NM:%s, MOBILE:%s, REG_DT:%s%n", userNo, userId, userPw, userNm, mobile, regDt);
+            }
+
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void test2() {
+        String userId = "USER03";
+        String userPw = "123456";
+        String userNm = "사용자03";
+        String mobile = "01000000000";
+
+        String sql = "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, MOBILE) VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
         } catch (SQLException e) {
             e.printStackTrace();
