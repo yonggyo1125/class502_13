0ce96968baaceb9535913cd19e6c831f650223e2
yonggyo1125 <yonggyo00@kakao.com>
2024-05-23 14:20:11 +0900

    수업 소스

--- 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex03.java
+++ 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex03.java
@@ -3,7 +3,17 @@ package exam01;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Ex03 {
+
+    /* DB 연결 URL, 계정 */
+    private String url = "jdbc:oracle:thin:@localhost:1521:XE";
+    private String user = "STUDY"; // SCOTT
+    private String password = "oracle"; // tiger
 
     @BeforeAll
     static void init() {
@@ -18,6 +28,12 @@ public class Ex03 {
 
     @Test
     void test1() {
+        String sql = "CALL REGISTER_MEMBER(?, ?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             CallableStatement cstmt = conn.prepareCall(sql)) {
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
