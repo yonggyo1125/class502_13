0ed8fbdfe2c105327d1d912ffc24df882e5fea7a
yonggyo1125 <yonggyo00@kakao.com>
2024-05-23 10:50:15 +0900

    수업 소스

--- 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex01.java
+++ 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex01.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Ex01 {
     @Test
@@ -20,8 +21,10 @@ public class Ex01 {
         String user = "STUDY"; // SCOTT
         String password = "oracle";  // tiger
 
-        try {
-            Connection conn = DriverManager.getConnection(url, user, password);
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement()) {
+
+            String sql = "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, MOBILE) VALUES (SEQ_MEMBER.NEXTVAL, 'USER01', '123456', '사용자01', '01000000000')";
 
         } catch (SQLException e) {
             e.printStackTrace();
