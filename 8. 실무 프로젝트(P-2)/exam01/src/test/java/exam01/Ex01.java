1a2e9bd16d164ad993d49412f17eaa77e18dcb72
yonggyo1125 <yonggyo00@kakao.com>
2024-05-23 11:01:44 +0900

    수업 소스

--- 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex01.java
+++ 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex01.java
@@ -2,10 +2,7 @@ package exam01;
 
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Ex01 {
     @Test
@@ -24,7 +21,33 @@ public class Ex01 {
         try (Connection conn = DriverManager.getConnection(url, user, password);
             Statement stmt = conn.createStatement()) {
 
-            String sql = "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, MOBILE) VALUES (SEQ_MEMBER.NEXTVAL, 'USER01', '123456', '사용자01', '01000000000')";
+            String sql = "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, MOBILE) VALUES (SEQ_MEMBER.NEXTVAL, 'USER02', '123456', '사용자02', '01000000000')";
+
+            int cnt = stmt.executeUpdate(sql);
+            System.out.println(cnt);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void test2() {
+        try {
+            // 오라클 드라이버 동적 로딩
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String url = "jdbc:oracle:thin:@localhost:1521:XE";
+        String user = "STUDY"; // SCOTT
+        String password = "oracle"; // tiger
+        try(Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement()) {
+
+            String sql = "SELECT * FROM MEMBER";
+            ResultSet resultSet = stmt.executeQuery(sql);
 
         } catch (SQLException e) {
             e.printStackTrace();
