7c7f54d7f710859b4a4804289903d3a3f9c54b49
yonggyo1125 <yonggyo00@kakao.com>
2024-05-23 12:03:38 +0900

    수업 소스

--- 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex02.java
+++ 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex02.java
@@ -3,6 +3,11 @@ package exam01;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Ex02 {
     private String url = "jdbc:oracle:thin:@localhost:1521:XE";
     private String user = "STUDY";
@@ -19,6 +24,17 @@ public class Ex02 {
 
     @Test
     void test1() {
+        String keyword = "사용자";
+        String sql = "SELECT * FROM MEMBER WHERE USER_NM LIKE ?";
+        try (Connection conn = DriverManager.getConnection(url, user,password);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
+            pstmt.setString(1, "%" + keyword + "%");
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
