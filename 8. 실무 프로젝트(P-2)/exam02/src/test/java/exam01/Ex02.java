f2f0e86f509d3a34838f984d864e2463b5fedf57
yonggyo1125 <yonggyo00@kakao.com>
2024-05-24 14:38:31 +0900

    수업 소스

--- 8. 실무 프로젝트(P-2)/exam02/src/test/java/exam01/Ex02.java
+++ 8. 실무 프로젝트(P-2)/exam02/src/test/java/exam01/Ex02.java
@@ -27,4 +27,19 @@ public class Ex02 {
         System.out.println(cnt);
 
     }
+
+    @Test
+    void test2() {
+        MemberMapper mapper = session.getMapper(MemberMapper.class);
+
+        Member member = Member.builder()
+                .userId("USER01")
+                .userPw("123456")
+                .userNm("(수정)사용자01")
+                .mobile("01000000000")
+                .build();
+
+        int cnt = session.update("mappers.MemberMapper.modify", member);
+        System.out.println(cnt);
+    }
 }
