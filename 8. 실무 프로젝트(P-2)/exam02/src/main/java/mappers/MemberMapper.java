32e9f01970dc08b41f64c41732f967f3fbf97af8
yonggyo1125 <yonggyo00@kakao.com>
2024-05-24 15:02:25 +0900

    수업 소스

--- 8. 실무 프로젝트(P-2)/exam02/src/main/java/mappers/MemberMapper.java
+++ 8. 실무 프로젝트(P-2)/exam02/src/main/java/mappers/MemberMapper.java
@@ -1,6 +1,7 @@
 package mappers;
 
 import member.Member;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -12,6 +13,12 @@ public interface MemberMapper {
     List<Member> getList2();
 
     int register(Member member);
+    int register2(Member member);
 
     int modify(Member member);
+
+    int delete(String userId);
+
+    @Delete("DELETE FROM MEMBER WHERE USER_ID = #{userId}")
+    int delete2(String userId);
 }
--- 8. 실무 프로젝트(P-2)/exam02/src/test/java/exam01/Ex02.java
+++ 8. 실무 프로젝트(P-2)/exam02/src/test/java/exam01/Ex02.java
@@ -22,10 +22,11 @@ public class Ex02 {
                 .userNm("사용자..")
                 .mobile("01000000000")
                 .build();
-        int cnt = mapper.register(member);
+       //int cnt = mapper.register(member);
+        int cnt = mapper.register2(member);
         //int cnt = session.insert("mappers.MemberMapper.register", member);
         System.out.println(cnt);
-
+        System.out.println(member);
     }
 
     @Test
@@ -39,7 +40,28 @@ public class Ex02 {
                 .mobile("01000000000")
                 .build();
 
-        int cnt = session.update("mappers.MemberMapper.modify", member);
+        //int cnt = session.update("mappers.MemberMapper.modify", member);
+        int cnt = mapper.modify(member);
         System.out.println(cnt);
+    }
+
+    @Test
+    void test3() {
+        Member member = Member.builder()
+                .userId("USER01")
+                .build();
+
+        int cnt = session.delete("mappers.MemberMapper.delete", member);
+        System.out.println(cnt);
+    }
+
+    @Test
+    void test4() {
+        MemberMapper mapper = session.getMapper(MemberMapper.class);
+        int cnt = mapper.delete("USER02");
+        int cnt2 = mapper.delete2("USER03");
+
+        System.out.println(cnt);
+        System.out.println(cnt2);
     }
 }
