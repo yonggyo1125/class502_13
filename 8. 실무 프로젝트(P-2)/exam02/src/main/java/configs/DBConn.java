6b7c3cd312219f956a0d9ce194d9798eb9b904c3
yonggyo1125 <yonggyo00@kakao.com>
2024-05-24 10:17:52 +0900

    수업 소스

--- 8. 실무 프로젝트(P-2)/exam02/src/main/java/configs/DBConn.java
+++ 8. 실무 프로젝트(P-2)/exam02/src/main/java/configs/DBConn.java
@@ -1,4 +1,20 @@
 package configs;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.io.IOException;
+import java.io.Reader;
+
 public class DBConn {
+    private static SqlSessionFactory factory;
+
+    static {
+        try {
+            Reader reader = Resources.getResourceAsReader("configs/mybatis-config.xml");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
