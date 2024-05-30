6fb26214135b0e2b5f5e037393f27da7220689db
yonggyo1125 <yonggyo00@kakao.com>
2024-05-30 12:00:41 +0900

    소켓 서버 구축

--- 8. 실무 프로젝트(P-2)/server/src/main/java/org/choongang/Server.java
+++ 8. 실무 프로젝트(P-2)/server/src/main/java/org/choongang/Server.java
@@ -1,7 +1,9 @@
 package org.choongang;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -19,5 +21,43 @@ public class Server {
         }
 
         threadPool = new ThreadPoolExecutor(2, 100, 120L, TimeUnit.SECONDS, new SynchronousQueue<>(){});
+    }
+
+    public void start() {
+
+        while(true) {
+            try {
+                Socket socket = serverSocket.accept();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class SocketHandler {
+        private Socket socket;
+
+        public SocketHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        // 수신 처리
+        public void input() {
+            threadPool.execute(() -> {
+                try(DataInputStream dis = new DataInputStream(socket.getInputStream())) {
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        // 전송 처리
+        public void output() {
+            threadPool.execute(() -> {
+
+            });
+        }
     }
 }
