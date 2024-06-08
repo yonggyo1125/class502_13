80e96e5746193e25d73a1d5b13ccb185e621c937
yonggyo1125 <yonggyo00@kakao.com>
2024-06-08 16:50:23 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어활용/보강/day02/src/exam02/Ex02.java
@@ -0,0 +1,12 @@
+package exam02;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Schedule2 s1 = new Schedule2();
+        s1.setYear(2024);
+        s1.setMonth(6);
+        s1.setDay(8);
+
+        System.out.println(s1); // s1.toString();
+    }
+}
