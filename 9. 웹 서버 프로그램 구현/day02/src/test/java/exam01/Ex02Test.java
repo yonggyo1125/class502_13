8b3f8cc0eb98b0206d069bbebc6b250b6354a25d
yonggyo1125 <yonggyo00@kakao.com>
2024-06-10 12:29:27 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day02/src/test/java/exam01/Ex02Test.java
+++ 9. 웹 서버 프로그램 구현/day02/src/test/java/exam01/Ex02Test.java
@@ -1,8 +1,29 @@
 package exam01;
 
+import com.github.javafaker.Faker;
+import global.Mailer;
+import member.services.LoginService;
+import member.validators.LoginValidator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
+import static org.mockito.Mockito.mock;
+
 public class Ex02Test {
+
+    private LoginService loginService;
+    private Mailer mailer;
+    private Faker faker;
+
+    @BeforeEach
+    void init() {
+        loginService = new LoginService(new LoginValidator());
+        mailer = mock(Mailer.class);
+        faker = new Faker(Locale.ENGLISH);
+    }
+
     @Test
     void test1() {
 
