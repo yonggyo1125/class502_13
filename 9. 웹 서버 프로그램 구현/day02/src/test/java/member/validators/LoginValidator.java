03d08c800ed9a1e4e78747e58a481425a9b87b9f
yonggyo1125 <yonggyo00@kakao.com>
2024-06-10 11:29:45 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day02/src/test/java/member/validators/LoginValidator.java
+++ 9. 웹 서버 프로그램 구현/day02/src/test/java/member/validators/LoginValidator.java
@@ -4,14 +4,16 @@ import global.exceptions.ValidationException;
 import global.validators.Validator;
 import jakarta.servlet.http.HttpServletRequest;
 
-public class LoginValidator implements Validator<HttpServletRequest> {
+public class LoginValidator implements Validator<HttpServletRequest>, RequiredValidator {
 
     @Override
     public void check(HttpServletRequest form) {
         String email = form.getParameter("email");
-        if (email == null || email.isBlank()) {
-            throw new ValidationException("이메일을 입력하세요.");
-        }
+        String password = form.getParameter("password");
 
+        /* 필수 항목 검증 S */
+        checkRequired(email, new ValidationException("아이디를 입력하세요."));
+        checkRequired(password, new ValidationException("비밀번호를 입력하세요."));
+        /* 필수 항목 검증 E */
     }
 }
