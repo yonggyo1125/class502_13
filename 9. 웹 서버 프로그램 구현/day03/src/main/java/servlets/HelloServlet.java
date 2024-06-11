8bb2279162d828d401443836008a8486dd2da46b
yonggyo1125 <yonggyo00@kakao.com>
2024-06-11 12:47:13 +0900

    수업 소스

---
+++ 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
@@ -0,0 +1,15 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
