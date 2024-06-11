0579779f08d61fc7c4f1902b613bc5dafb274b71
yonggyo1125 <yonggyo00@kakao.com>
2024-06-11 15:39:46 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
+++ 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,10 +14,24 @@ public class HelloServlet extends HttpServlet {
         //System.out.println("안녕하세요~!");
         System.out.println("doGet()");
     }
-
+    /*
     @Override
     public void init() throws ServletException {
         System.out.println("init()");
+    }
+    */
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        // HelloServlet만 조회 가능
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s%n", key1, key2);
+
+        // 모든 서블릿이 조회 가능
+        String common1 = config.getInitParameter("common1");
+        String common2 = config.getInitParameter("common2");
+        System.out.printf("common1=%s, common2=%s%n", common1, common2);
     }
 
     @Override
