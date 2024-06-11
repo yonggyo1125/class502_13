d235c6ddcc6cc1f1ac660535b124aa6801aba292
yonggyo1125 <yonggyo00@kakao.com>
2024-06-11 15:16:25 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
+++ 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
@@ -10,6 +10,17 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("안녕하세요~!");
+        //System.out.println("안녕하세요~!");
+        System.out.println("doGet()");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init()");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy()");
     }
 }
