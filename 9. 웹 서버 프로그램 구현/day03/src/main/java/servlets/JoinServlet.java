623d02ca1ac5c46037e403cbf08c5e736206c864
yonggyo1125 <yonggyo00@kakao.com>
2024-06-11 16:23:24 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/JoinServlet.java
+++ 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/JoinServlet.java
@@ -12,11 +12,18 @@ public class JoinServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.setContentType("text/html");
-        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        //resp.setCharacterEncoding("UTF-8");
 
         PrintWriter out = resp.getWriter();
         out.println("<h1>회원가입</h1>");
+        out.println("<form method='POST' action='join'>");
+        out.println("이메일:<input type='text' name='email'><br>");
+        out.println("비밀번호:<input type='password' name='password'><br>");
+        out.println("비밀번호 확인:<input type='password' name='confirmPassword'><br>");
+        out.println("회원명:<input type='text' name='userName'><br>");
+        out.println("<button type='submit'>가입하기</button>");
+        out.println("</form>");
     }
 
     @Override
