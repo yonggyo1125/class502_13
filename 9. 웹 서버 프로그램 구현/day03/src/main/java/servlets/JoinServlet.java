c0ff1c9953522c12aa0f8fa09b0fda2583aad261
yonggyo1125 <yonggyo00@kakao.com>
2024-06-11 16:02:36 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/JoinServlet.java
+++ 9. 웹 서버 프로그램 구현/day03/src/main/java/servlets/JoinServlet.java
@@ -6,11 +6,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class JoinServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>회원가입</h1>");
     }
 
     @Override
