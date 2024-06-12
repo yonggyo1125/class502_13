24c2134ece391ab5b93c39c1e0a6eeb23c5b4731
yonggyo1125 <yonggyo00@kakao.com>
2024-06-12 10:41:36 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day04/src/main/java/controllers/MemberController.java
+++ 9. 웹 서버 프로그램 구현/day04/src/main/java/controllers/MemberController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/member/*")
 public class MemberController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
