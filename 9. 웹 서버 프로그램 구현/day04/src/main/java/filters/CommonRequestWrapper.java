c4f32a3afac477e3f60829a92fbe4166b6892e93
yonggyo1125 <yonggyo00@kakao.com>
2024-06-12 12:47:48 +0900

    수업 소스

---
+++ 9. 웹 서버 프로그램 구현/day04/src/main/java/filters/CommonRequestWrapper.java
@@ -0,0 +1,15 @@
+package filters;
+
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+
+public class CommonRequestWrapper extends HttpServletRequestWrapper {
+
+    public CommonRequestWrapper(ServletRequest request) {
+       super((HttpServletRequest) request);
+
+       // 요청 전 공통 처리 부분
+        System.out.println("요청 전 공통 처리 코드....");
+    }
+}
