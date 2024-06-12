e04528c63fba317bc66c9108a5f0dad2a6e7cdfc
yonggyo1125 <yonggyo00@kakao.com>
2024-06-12 12:25:37 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day04/src/main/java/filters/CommonFilter.java
+++ 9. 웹 서버 프로그램 구현/day04/src/main/java/filters/CommonFilter.java
@@ -5,6 +5,20 @@ import jakarta.servlet.*;
 import java.io.IOException;
 
 public class CommonFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("init()");
+        String key1 = filterConfig.getInitParameter("key1");
+        String key2 = filterConfig.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s%n", key1, key2);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy()");
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
