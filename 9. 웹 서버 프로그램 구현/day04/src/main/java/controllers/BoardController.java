606002bf6b1e24a489eea276f6f8105cc72aabb3
yonggyo1125 <yonggyo00@kakao.com>
2024-06-12 10:48:54 +0900

    수업 소스

---
+++ 9. 웹 서버 프로그램 구현/day04/src/main/java/controllers/BoardController.java
@@ -0,0 +1,8 @@
+package controllers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/board")
+public class BoardController extends HttpServlet {
+}
