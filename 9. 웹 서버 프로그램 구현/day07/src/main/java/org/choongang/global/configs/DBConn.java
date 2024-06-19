7636148958d0f47e85dcbf7bbb347a1715cd8b1c
yonggyo1125 <yonggyo00@kakao.com>
2024-06-19 10:19:54 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/global/configs/DBConn.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/global/configs/DBConn.java
@@ -1,14 +1,32 @@
 package org.choongang.global.configs;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import java.io.IOException;
 import java.io.Reader;
 
 public class DBConn {
     private static SqlSessionFactory factory;
 
     static {
-        Reader reader = Resources.getResourceAsReader("org/choongang/global/configs/mybatis-config.xml");
+        try {
+            Reader reader = Resources.getResourceAsReader("org/choongang/global/configs/mybatis-config.xml");
+
+            factory = new SqlSessionFactoryBuilder().build(reader);
+
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+    }
+
+    public static SqlSession getSession(boolean autoCommit) {
+        return factory.openSession(autoCommit);
+    }
+
+    public static SqlSession sqlSession() {
+        return getSession(true);
     }
 }
