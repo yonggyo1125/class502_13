747f902a66aa61fda897eca22cb08c3f7aded54a
yonggyo1125 <yonggyo00@kakao.com>
2024-06-19 10:31:32 +0900

    수업 소스

---
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/configs/DBConnTest.java
@@ -0,0 +1,20 @@
+package org.choongang.configs;
+
+import org.apache.ibatis.session.SqlSession;
+import org.choongang.global.configs.DBConn;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class DBConnTest {
+    
+    @Test
+    @DisplayName("DB 연결 테스트")
+    void dbConnectionTest() {
+        assertDoesNotThrow(() -> {
+            SqlSession session = DBConn.sqlSession();
+            System.out.println(session);
+        });
+    }
+}
