26699c58dc1a2a1a168745a8d3343a09db842c52
yonggyo1125 <yonggyo00@kakao.com>
2024-06-19 11:58:13 +0900

    수업 소스

---
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/global/exceptions/CommonException.java
@@ -0,0 +1,17 @@
+package org.choongang.global.exceptions;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CommonException extends RuntimeException {
+
+    private int status; // 응답 코드
+
+    public CommonException(String message) {
+        this(message, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    public CommonException(String message, int status) {
+        super(message);
+        this.status = status;
+    }
+}
