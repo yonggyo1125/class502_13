a2d5830b26297e33cbaebddf94384d35240217dd
yonggyo1125 <yonggyo00@kakao.com>
2024-06-19 14:13:28 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/global/validators/RequiredValidator.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/global/validators/RequiredValidator.java
@@ -6,4 +6,11 @@ public interface RequiredValidator {
             throw e;
         }
     }
+
+    // 참인지 체크
+    default void checkTrue(boolean checked, RuntimeException e) {
+        if (!checked) {
+            throw e;
+        }
+    }
 }
--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/JoinValidator.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/validators/JoinValidator.java
@@ -1,13 +1,26 @@
 package org.choongang.member.validators;
 
+import org.choongang.global.exceptions.BadRequestException;
+import org.choongang.global.validators.RequiredValidator;
 import org.choongang.global.validators.Validator;
 import org.choongang.member.controllers.RequestJoin;
 
-public class JoinValidator implements Validator<RequestJoin> {
+public class JoinValidator implements Validator<RequestJoin>, RequiredValidator {
 
     @Override
     public void check(RequestJoin form) {
+        String email = form.getEmail();
+        String password = form.getPassword();
+        String confirmPassword = form.getConfirmPassword();
+        String userName = form.getUserName();
+        boolean termsAgree = form.isTermsAgree();
+
         /* 필수 항목 검증 - 이메일, 비밀번호, 비밀번호 확인, 회원명, 약관 동의 */
+        checkRequired(email, new BadRequestException("아이디를 입력하세요."));
+        checkRequired(password, new BadRequestException("비밀번호를 입력하세요."));
+        checkRequired(confirmPassword, new BadRequestException("비밀번호를 확인하세요."));
+        checkRequired(userName, new BadRequestException("회원명을 입력하세요."));
+        checkTrue(termsAgree, new BadRequestException("약관에 동의하세요."));
 
     }
 }
