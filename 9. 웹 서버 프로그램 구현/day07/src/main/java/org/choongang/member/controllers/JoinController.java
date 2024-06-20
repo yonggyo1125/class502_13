53cc91bf062c44becf52f77289f45ffeaff70299
yonggyo1125 <yonggyo00@kakao.com>
2024-06-20 15:01:25 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/JoinController.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/JoinController.java
@@ -7,10 +7,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.choongang.global.exceptions.CommonException;
 import org.choongang.member.services.JoinService;
 import org.choongang.member.services.MemberServiceProvider;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/member/join")
 public class JoinController extends HttpServlet {
@@ -23,8 +25,14 @@ public class JoinController extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        JoinService service = MemberServiceProvider.getInstance().joinService();
-        service.process(req);
+        try {
+            JoinService service = MemberServiceProvider.getInstance().joinService();
+            service.process(req);
+        } catch (CommonException e) {
+            resp.setContentType("text/html; charset=UTF-8");
+            resp.setStatus(e.getStatus());
+            PrintWriter out = resp.getWriter();
+            out.printf("<script>alert('%s');</script>", e.getMessage());
+        }
     }
 }
