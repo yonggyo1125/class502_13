8c81462d54ee205589ea8b1a9725d51547dfabb5
yonggyo1125 <yonggyo00@kakao.com>
2024-06-20 10:23:56 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/tests/LoginServiceTest.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/tests/LoginServiceTest.java
@@ -1,32 +1,56 @@
 package org.choongang.member.tests;
 
+import com.github.javafaker.Faker;
+import jakarta.servlet.http.HttpServletRequest;
 import org.choongang.member.services.LoginService;
 import org.choongang.member.services.MemberServiceProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Locale;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("로그인 기능 테스트")
 public class LoginServiceTest {
 
     private LoginService loginService;
+    private Faker faker;
+
+    @Mock
+    private HttpServletRequest request;
 
     @BeforeEach
     void init() {
         loginService = MemberServiceProvider.getInstance().loginService();
+
+        faker = new Faker(Locale.ENGLISH);
+
+        setParam("email", faker.internet().emailAddress());
+        setParam("password", faker.regexify("\\w{8}").toLowerCase());
+    }
+
+    void setParam(String name, String value) {
+        given(request.getParameter(name)).willReturn(value);
     }
 
     @Test
     @DisplayName("로그인 성공시 예외가 발생하지 않음")
     void successTest() {
         assertDoesNotThrow(() -> {
-            LoginService loginService = new LoginService();
             loginService.process();
         });
+    }
+
+    @Test
+    @DisplayName("필수 입력 항목(이메일, 비밀번호) 검증, 검증 실패시 BadRequestException 발생")
+    void requiredFieldTest() {
+
     }
 }
