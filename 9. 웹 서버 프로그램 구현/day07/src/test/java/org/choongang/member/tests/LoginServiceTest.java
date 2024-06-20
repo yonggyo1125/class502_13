e346354bfde325934b48d3595cbfabfd66d0daea
yonggyo1125 <yonggyo00@kakao.com>
2024-06-20 09:52:54 +0900

    수업 소스

---
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/tests/LoginServiceTest.java
@@ -0,0 +1,17 @@
+package org.choongang.member.tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("로그인 기능 테스트")
+public class LoginServiceTest {
+    
+    @Test
+    @DisplayName("로그인 성공시 예외가 발생하지 않음")
+    void successTest() {
+        
+    }
+}
