0e27f3ac90e2ab986f04f80b4f8a33a70e6b245d
yonggyo1125 <yonggyo00@kakao.com>
2024-06-21 11:58:42 +0900

    수업 소스

--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/file/controllers/FileUploadController.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/file/controllers/FileUploadController.java
@@ -6,8 +6,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload2.core.DiskFileItem;
+import org.apache.commons.fileupload2.jakarta.servlet6.JakartaServletDiskFileUpload;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
 
 @WebServlet("/file/upload")
 public class FileUploadController extends HttpServlet {
@@ -20,6 +24,25 @@ public class FileUploadController extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JakartaServletDiskFileUpload upload = new JakartaServletDiskFileUpload();
 
+        // multipart 형식 body 데이터를 일반 양식과 파일 데이터로 분리해서 조회 가능한 List 형태로 변환
+        List<DiskFileItem> items = upload.parseRequest(req);
+        for (DiskFileItem item : items) {
+
+            if (item.isFormField()) { // 일반 텍스트 형태의 양식 데이터
+                // subject, content
+                String name = item.getFieldName();
+                String value = item.getString(Charset.forName("UTF-8"));
+                System.out.printf("name=%s, value=%s%n", name, value);
+            } else { // 파일 데이터 - file
+                String fileName = item.getName(); // 파일 이름
+                String contentType = item.getContentType(); // 파일 형식
+                long size = item.getSize(); // 파일 용량
+
+                System.out.printf("파일이름: %s, 파일형식: %s, 파일용량: %d%n", fileName, contentType, size);
+
+            }
+        }
     }
 }
