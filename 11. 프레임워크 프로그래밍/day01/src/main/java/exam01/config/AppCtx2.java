6c070bffed4c4354a25e8bcc7ae25184403a8f5a
yonggyo1125 <yonggyo00@kakao.com>
2024-07-05 11:24:56 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/config/AppCtx2.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/config/AppCtx2.java
@@ -1,22 +1,28 @@
-package exam01.member;
+package exam01.config;
 
 import exam01.member.dao.MemberDao;
 import exam01.member.services.JoinService;
 import exam01.member.validators.JoinValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class Assembler {
+@Configuration
+public class AppCtx2 {
 
+    @Bean
     public MemberDao memberDao() {
         return new MemberDao();
     }
 
+    @Bean
     public JoinValidator joinValidator() {
-        JoinValidator validator = new JoinValidator();
-        validator.setMemberDao(memberDao());
+        JoinValidator joinValidator = new JoinValidator();
+        joinValidator.setMemberDao(memberDao());
 
-        return validator;
+        return joinValidator;
     }
 
+    @Bean
     public JoinService joinService() {
         return new JoinService(joinValidator(), memberDao());
     }
--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/validators/JoinValidator.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/validators/JoinValidator.java
@@ -2,8 +2,15 @@ package exam01.member.validators;
 
 import exam01.global.validators.Validator;
 import exam01.member.controllers.RequestJoin;
+import exam01.member.dao.MemberDao;
 
 public class JoinValidator implements Validator<RequestJoin> {
+
+    private MemberDao memberDao;
+
+    public void setMemberDao(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
 
     @Override
     public void check(RequestJoin form) {
