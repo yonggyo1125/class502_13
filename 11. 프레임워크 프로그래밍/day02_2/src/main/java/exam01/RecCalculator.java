f9faa6d12ca7e16e8aec8fdbac24950642894829
yonggyo1125 <yonggyo00@kakao.com>
2024-07-08 14:18:35 +0900

    수업 소스

---
+++ 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam01/RecCalculator.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class RecCalculator implements Calculator {
+
+    @Override
+    public long factorial(long num) {
+        if (num < 1L) {
+            return 1L;
+        }
+
+        return num * factorial(num - 1L);
+    }
+}
