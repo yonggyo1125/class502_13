f0fd820081b3d2961bf523e24e471238d0210623
yonggyo1125 <yonggyo00@kakao.com>
2024-07-08 14:43:02 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day02_2/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day02_2/src/test/java/exam01/Ex01.java
@@ -25,4 +25,20 @@ public class Ex01 {
         etime = System.nanoTime();  // 공통 기능
         System.out.printf("cal2 걸린 시간:%d%n", etime - stime);
     }
+
+    @Test
+    void test2() {
+
+        ProxyCalculator cal1 = new ProxyCalculator(new ImplCalculator());
+
+        long result1 = cal1.factorial(10L);
+        System.out.printf("cal1: %d%n", result1);
+
+
+        ProxyCalculator cal2 = new ProxyCalculator(new RecCalculator());
+
+        long result2 = cal2.factorial(10L);
+        System.out.printf("cal2: %d%n", result2);
+
+    }
 }
