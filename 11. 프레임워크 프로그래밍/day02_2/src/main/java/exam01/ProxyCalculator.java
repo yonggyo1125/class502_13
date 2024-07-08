f3c193885c9e55ba90894f53842f2d676475e0a3
yonggyo1125 <yonggyo00@kakao.com>
2024-07-08 14:46:12 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam01/ProxyCalculator.java
+++ 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam01/ProxyCalculator.java
@@ -13,9 +13,17 @@ public class ProxyCalculator implements Calculator {
     @Override
     public long factorial(long num) {
 
-        // 다른 계산기의 핵심 기능을 대신 수행
-        long result = calculator.factorial(num); // 다른 계산기의 factorial 연산을 대신 수행
+        long stime = System.nanoTime(); // 공통 기능
 
-        return result;
+        try {
+            // 다른 계산기의 핵심 기능을 대신 수행
+            long result = calculator.factorial(num); // 다른 계산기의 factorial 연산을 대신 수행
+
+            return result;
+
+        } finally {
+            long etime = System.nanoTime();   // 공통 기능
+            System.out.printf("걸린시간: %d%n", etime - stime);
+        }
     }
 }
