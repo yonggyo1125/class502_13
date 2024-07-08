6f5447f52116490a009aaf03a14c07ad6916981f
yonggyo1125 <yonggyo00@kakao.com>
2024-07-08 15:16:09 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam02/CaculatorHandler.java
+++ 11. 프레임워크 프로그래밍/day02_2/src/main/java/exam02/CaculatorHandler.java
@@ -2,10 +2,24 @@ package exam02;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class CaculatorHandler implements InvocationHandler {
+    /**
+     *
+     * @param method : 호출한 메서드의 정보
+     *                 동적 메서드 호출 method.invoke(...)
+     *
+     *        args : 메서드 호출시 넘겨준 값(인수)
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        System.out.println(method);
+        System.out.println(Arrays.toString(args));
+
         return null;
     }
 }
