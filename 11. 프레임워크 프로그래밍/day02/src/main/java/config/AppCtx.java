c8cf4f8cf5782299347777b7535918bc4fb92ff7
yonggyo1125 <yonggyo00@kakao.com>
2024-07-08 11:34:28 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
@@ -1,10 +1,14 @@
 package config;
 
+import global.annotations.ManualBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan("member")
+@ComponentScan(basePackages="member",
+    excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes= ManualBean.class)
+)
 public class AppCtx {
 
 
