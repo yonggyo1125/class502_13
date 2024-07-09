106c4ffe6c0750f394b4a1b19451f20b2a57470e
yonggyo1125 <yonggyo00@kakao.com>
2024-07-09 15:43:11 +0900

    강의 소스

--- 11. 프레임워크 프로그래밍/day03_2/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day03_2/src/test/java/exam01/Ex01.java
@@ -1,12 +1,18 @@
 package exam01;
 
 import config.AppCtx;
+import member.entities.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = AppCtx.class)
@@ -22,5 +28,21 @@ public class Ex01 {
         int result = jdbcTemplate.update(sql, "user03@test.org", "123456", "사용자03");
 
         System.out.println(result);
+    }
+
+    @Test
+    void test2() {
+        List<Member> members = jdbcTemplate.query("SELECT * FROM MEMBER", new RowMapper<Member>() {
+            @Override
+            public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return Member.builder()
+                        .seq(rs.getLong("SEQ"))
+                        .email(rs.getString("EMAIL"))
+                        .password(rs.getString("PASSWORD"))
+                        .userName(rs.getString("USER_NAME"))
+                        .regDt(rs.getTimestamp("REG_DT").toLocalDateTime())
+                        .build();
+            }
+        });
     }
 }
