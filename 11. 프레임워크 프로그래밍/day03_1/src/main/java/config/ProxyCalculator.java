9d77ea354d36e834055296815fdfae6865489d5d
yonggyo1125 <yonggyo00@kakao.com>
2024-07-09 10:18:31 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/ProxyCalculator.java
+++ 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/ProxyCalculator.java
@@ -1,5 +1,7 @@
 package config;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
@@ -8,4 +10,14 @@ public class ProxyCalculator {
 
     @Pointcut("execution(* exam01..*(..))")
     public void publicTarget() {}
+
+    @Around("publicTarget()")
+    public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        System.out.println("유입!");
+
+        Object result = joinPoint.proceed(); // 핵심 기능 대신 실행 - factorial
+
+        return result;
+    }
 }
