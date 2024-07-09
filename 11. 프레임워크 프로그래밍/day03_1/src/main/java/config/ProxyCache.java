876ff35cd76342d7aa362e5e2925c1182c6a77a2
yonggyo1125 <yonggyo00@kakao.com>
2024-07-09 12:16:19 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/ProxyCache.java
+++ 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/ProxyCache.java
@@ -14,11 +14,13 @@ import java.util.Map;
 public class ProxyCache {
 
     private Map<Long, Object> data = new HashMap<>();
-
+    /*
     @Pointcut("execution(* exam01..*(..))")
     public void publicTarget() {}
 
     @Around("publicTarget()")
+     */
+    @Around("config.CommonPointcut.publicTarget()")
     public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
 
         Object[] args = joinPoint.getArgs();
