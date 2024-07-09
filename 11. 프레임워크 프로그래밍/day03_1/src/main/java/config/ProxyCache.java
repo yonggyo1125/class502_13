5c46fafe86b2210f0531f90e8c8b3c4eca0e888e
yonggyo1125 <yonggyo00@kakao.com>
2024-07-09 11:52:56 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/ProxyCache.java
+++ 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/ProxyCache.java
@@ -5,15 +5,33 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Aspect
 public class ProxyCache {
+
+    private Map<Long, Object> data = new HashMap<>();
+
     @Pointcut("execution(* exam01..*(..))")
     public void publicTarget() {}
 
     @Around("publicTarget()")
     public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
 
+        Object[] args = joinPoint.getArgs();
+        Long num = (Long)args[0];
+        if (data.containsKey(num)) { // 이미 캐시에 저장된 결과 값이 있으면
+
+            System.out.println("캐시 사용..");
+
+            return data.get(num);
+        }
+
+
         Object result = joinPoint.proceed();
+        data.put(num, result); // 캐시에 저장
+        System.out.println("캐시에 저장...");
 
         return result;
     }
