c3ec3d88b15e9a807a8c29694d09d0e317a45bfb
yonggyo1125 <yonggyo00@kakao.com>
2024-07-09 10:26:16 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day03_1/src/main/java/config/AppCtx.java
@@ -4,9 +4,16 @@ import exam01.Calculator;
 import exam01.RecCalculator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy  // AOP 자동 설정 애노테이션
 public class AppCtx {
+
+    @Bean
+    public ProxyCalculator proxyCalculator() {
+        return new ProxyCalculator();
+    }
 
     @Bean
     public Calculator calculator() {
