571991482f08f854fca8515eb7b4fc730d009163
yonggyo1125 <yonggyo00@kakao.com>
2024-07-09 15:17:51 +0900

    강의 소스

---
+++ 11. 프레임워크 프로그래밍/day03_2/src/test/java/exam01/Ex01.java
@@ -0,0 +1,26 @@
+package exam01;
+
+import config.AppCtx;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppCtx.class)
+public class Ex01 {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void test1() {
+        String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME) " +
+                    " VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)";
+        int result = jdbcTemplate.update(sql, "user01@test.org", "123456", "사용자01");
+
+        System.out.println(result);
+    }
+}
