7cffcb818d523e3935f089b8cba5e3df0ecb5f71
yonggyo1125 <yonggyo00@kakao.com>
2024-07-10 11:34:32 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day04/src/main/java/mappers/member/MemberMapper.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/mappers/member/MemberMapper.java
@@ -1,8 +1,13 @@
 package mappers.member;
 
+import member.entities.Member;
 import org.apache.ibatis.annotations.Select;
 
 public interface MemberMapper {
     @Select("SELECT COUNT(*) FROM MEMBER")
     long getTotal();
+
+    int register(Member member);
+    Member get(String email);
+    int exists(String email);
 }
--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex03.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex03.java
@@ -2,12 +2,15 @@ package exam01;
 
 import config.AppCtx;
 import mappers.member.MemberMapper;
+import member.entities.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = AppCtx.class)
 public class Ex03 {
@@ -19,5 +22,22 @@ public class Ex03 {
     void test1() {
         long total = memberMapper.getTotal();
         System.out.println(total);
+    }
+
+    @Test
+    void test2() {
+        Member member = Member.builder()
+                .email("user99@test.org")
+                .password("12345678")
+                .userName("사용자99")
+                .build();
+        int result = memberMapper.register(member);
+
+        Member member2 = memberMapper.get(member.getEmail());
+        System.out.println(member2);
+
+        int exists = memberMapper.exists(member.getEmail());
+        System.out.println(exists);
+
     }
 }
