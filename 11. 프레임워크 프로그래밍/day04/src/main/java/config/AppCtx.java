11d6196f0e62de58e94102dba2b8d86cb86f52a0
yonggyo1125 <yonggyo00@kakao.com>
2024-07-10 09:14:41 +0900

    수업 소스

---
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/config/AppCtx.java
@@ -0,0 +1,36 @@
+package config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class AppCtx {
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+        /* DB 연결 설정 S */
+        ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        ds.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        ds.setUsername("SPRING");
+        ds.setPassword("oracle");
+        /* DB 연결 설정 E */
+
+        /* 커넥션 풀 설정 S */
+        ds.setTestWhileIdle(true); // 유휴 객체 유효성 체크
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        ds.setTimeBetweenEvictionRunsMillis(10 * 1000); // 10초에 한번씩 연결 상태 체크
+        ds.setMinEvictableIdleTimeMillis(1000 * 60); // 유휴 객체 생존 시간 1분
+        /* 커넥션 풀 설정 E */
+
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
---
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/member/entities/Member.java
@@ -0,0 +1,16 @@
+package member.entities;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Member {
+    private long seq;
+    private String email;
+    private String password;
+    private String userName;
+    private LocalDateTime regDt;
+}
