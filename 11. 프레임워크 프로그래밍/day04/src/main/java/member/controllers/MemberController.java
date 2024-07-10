7b53431f5a3927b4f07d55fa6a1f577726552c4d
yonggyo1125 <yonggyo00@kakao.com>
2024-07-10 16:01:50 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day04/src/main/java/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/member/controllers/MemberController.java
@@ -1,15 +1,39 @@
 package member.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
 
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
+
+
+    @Autowired
+    private HttpSession session;
+
     @GetMapping("/member/join")
     public String join(@RequestParam(name="name", defaultValue="기본값") String name) {
         System.out.println("name:" + name);
         return "member/join";
-     }
+    }
+
+    @PostMapping("/member/join")
+    public String joinPs(RequestJoin form, HttpServletRequest request, HttpServletResponse response, HttpSession session, Model model, Errors errors) {
+        System.out.println(form);
+
+        return "member/join";
+    }
 }
