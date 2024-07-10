38ac06569e2fbec6460eaffe53698efdbbbe9690
yonggyo1125 <yonggyo00@kakao.com>
2024-07-10 12:31:52 +0900

    수업 소스

---
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex04.java
@@ -0,0 +1,32 @@
+package exam01;
+
+import config.AppCtx;
+import member.controllers.RequestJoin;
+import member.services.JoinService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppCtx.class)
+public class Ex04 {
+
+    @Autowired
+    private JoinService service;
+
+    @Test
+    void test1() {
+        RequestJoin form = new RequestJoin();
+        form.setEmail("user99@test.org");
+        form.setPassword("12345678");
+        form.setConfirmPassword(form.getPassword());
+        form.setUserName("사용자99");
+        form.setAgree(true);
+
+        service.process(form);
+    }
+}
