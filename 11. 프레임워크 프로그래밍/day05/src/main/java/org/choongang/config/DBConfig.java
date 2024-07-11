ce0c40e2f69aea4302bf8e5b6cad23d17a022ec8
yonggyo1125 <yonggyo00@kakao.com>
2024-07-11 10:48:25 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
@@ -1,9 +1,31 @@
 package org.choongang.config;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DBConfig {
 
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
 
+        /* 연결 설정 S */
+        ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        ds.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        ds.setUsername("SPRING");
+        ds.setPassword("oracle");
+        /* 연결 설정 E */
+
+        /* 커넥션 풀 설정 S */
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        ds.setTestWhileIdle(true);
+        ds.setMinEvictableIdleTimeMillis(1000 * 60);
+        ds.setTimeBetweenEvictionRunsMillis(1000 * 5);
+        /* 커넥션 풀 설정 E */
+
+        return ds;
+    }
 }
