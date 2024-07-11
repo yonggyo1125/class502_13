6692eebb7d385185223cdfa684f1660f876d5d17
yonggyo1125 <yonggyo00@kakao.com>
2024-07-11 11:48:21 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/repositories/MemberRepositoryTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/repositories/MemberRepositoryTest.java
@@ -1,4 +1,24 @@
 package org.choongang.member.repositories;
 
+import org.choongang.config.MvcConfig;
+import org.choongang.member.entities.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+import java.util.List;
+
+@SpringJUnitWebConfig
+@ContextConfiguration(classes = MvcConfig.class)
 public class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository repository;
+
+    @Test
+    void test1() {
+        List<Member> members = (List<Member>) repository.findAll();
+        members.forEach(System.out::println);
+    }
 }
