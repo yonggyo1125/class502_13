920f344e064be2d8feb34fdf3dd6643ff6f5c1e7
yonggyo1125 <yonggyo00@kakao.com>
2024-07-11 10:54:11 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
@@ -1,13 +1,19 @@
 package org.choongang.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
+@MapperScan("org.choongang")
 public class DBConfig {
 
     @Bean(destroyMethod = "close")
@@ -40,5 +46,13 @@ public class DBConfig {
     @Bean
     public PlatformTransactionManager transactionManager() {
         return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+
+        return factoryBean.getObject();
     }
 }
