af787019579458a81304aa5819f78a9cc7c13334
yonggyo1125 <yonggyo00@kakao.com>
2024-07-11 16:04:31 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -4,24 +4,26 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping
+@RequestMapping("/member")
 public class MemberController {
 
-    //@GetMapping("/member/join")
-    @RequestMapping(path="/member/join", method = {RequestMethod.GET, RequestMethod.POST})
-    public String join(Model model, HttpServletRequest request) {
-
-        model.addAttribute("message", "안녕하세요.");
-        System.out.println("method : " + request.getMethod());
+    @GetMapping("/join")
+    public String join() {
 
         return "member/join";
     }
 
+    @PostMapping("/join")
+    public String joinPs(RequestJoin form) {
+
+        return "redirect:/member/login";
+    }
 
     /*
     @GetMapping("/member/join")
