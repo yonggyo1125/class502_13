5f13c812507f2f01b5eaaa4fe4d20f24815a9c5c
yonggyo1125 <yonggyo00@kakao.com>
2024-07-11 10:28:18 +0900

    수업 소스

---
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
@@ -0,0 +1,23 @@
+package org.choongang.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.choongang")
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/templates/", ".jsp");
+    }
+}
---
+++ 11. 프레임워크 프로그래밍/day05/src/main/webapp/WEB-INF/templates/member/join.jsp
@@ -0,0 +1,2 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<h1>회원 가입</h1>
