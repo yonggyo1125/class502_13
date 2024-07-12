c9a7d48f0f04511c7b68848406fd359e916b9c68
yonggyo1125 <yonggyo00@kakao.com>
2024-07-12 09:34:22 +0900

    수업 소스

---
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/controllers/MemberControllerTest.java
@@ -0,0 +1,10 @@
+package org.choongang.member.controllers;
+
+import org.choongang.config.MvcConfig;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+@SpringJUnitWebConfig
+@ContextConfiguration(classes = MvcConfig.class)
+public class MemberControllerTest {
+}
