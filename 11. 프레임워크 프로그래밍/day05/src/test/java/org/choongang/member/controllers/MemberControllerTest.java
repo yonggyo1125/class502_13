66c880a757402c0278d34c32b142ef4835cb59cd
yonggyo1125 <yonggyo00@kakao.com>
2024-07-12 09:45:23 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/controllers/MemberControllerTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/controllers/MemberControllerTest.java
@@ -1,10 +1,36 @@
 package org.choongang.member.controllers;
 
 import org.choongang.config.MvcConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringJUnitWebConfig
 @ContextConfiguration(classes = MvcConfig.class)
 public class MemberControllerTest {
+
+    @Autowired
+    private WebApplicationContext ctx;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void init() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+
+    @Test
+    void test1() throws Exception {
+        mockMvc.perform(post("/member/join"))
+                .andDo(print());
+    }
+
 }
