9b090a5213b80d77f8c965da6534e718b70425d2
yonggyo1125 <yonggyo00@kakao.com>
2024-07-12 09:27:57 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,23 +1,35 @@
 package org.choongang.member.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
+    //private final Logger log = LoggerFactory.getLogger(MemberController.class);
+
     @GetMapping("/join")
-    public String join() {
+    public String join1() {
+
+        log.info("{}, {} 없음", "mode1", "mode2");
 
         return "member/join";
     }
+
+    @GetMapping(path="/join", params={"mode=join"})
+    public String join() {
+
+        log.info("mode=join");
+
+        return "member/join";
+    }
+
+
 
     @PostMapping("/join")
     public String joinPs(RequestJoin form) {
