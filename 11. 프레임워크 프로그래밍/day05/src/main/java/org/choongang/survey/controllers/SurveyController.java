01b0c39e66876cf93c7439e610ed82403ab82763
yonggyo1125 <yonggyo00@kakao.com>
2024-07-16 11:00:20 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/survey/controllers/SurveyController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/survey/controllers/SurveyController.java
@@ -1,17 +1,20 @@
 package org.choongang.survey.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/survey")
+@SessionAttributes("requestSurvey")
 public class SurveyController {
 
+    @ModelAttribute
+    public RequestSurvey requestSurvey() {
+        return new RequestSurvey();
+    }
+
     @GetMapping("/step1")
-    public String step1(@ModelAttribute RequestSurvey form) {
+    public String step1() {
 
         return "survey/step1";
     }
