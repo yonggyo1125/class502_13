3d032c6b92cae0669cb222947ead82cf27ceab0e
yonggyo1125 <yonggyo00@kakao.com>
2024-07-16 09:52:19 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/interceptors/MemberOnlyInterceptor.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/interceptors/MemberOnlyInterceptor.java
@@ -2,6 +2,7 @@ package org.choongang.global.interceptors;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -15,11 +16,19 @@ public class MemberOnlyInterceptor implements HandlerInterceptor {
 
         log.info("preHandle()");
 
-        return true;
+        HttpSession session = request.getSession();
+        if (session.getAttribute("member") != null) { // 로그인 상태인 경우
+            return true; // 컨트롤러 빈 메서드 실행
+        }
+
+        response.sendRedirect(request.getContextPath() + "/member/login");
+
+        return false; // 미로그인 상태
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        modelAndView.addObject("message", "postHandle!");
 
         log.info("postHandle()");
     }
--- 11. 프레임워크 프로그래밍/day05/src/main/webapp/WEB-INF/templates/mypage/index.jsp
+++ 11. 프레임워크 프로그래밍/day05/src/main/webapp/WEB-INF/templates/mypage/index.jsp
@@ -1,2 +1,3 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
 <h1>마이페이지</h1>
+<h2>${message}</h2>
