a1cb9aafcd94a647138f4f0c3b38fef770248327
yonggyo1125 <yonggyo00@kakao.com>
2024-07-17 12:29:03 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
@@ -1,6 +1,7 @@
 package org.choongang.member.api.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.choongang.member.entities.Member;
 import org.choongang.member.mappers.MemberMapper;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
@@ -24,9 +30,29 @@ public class ApiMemberController {
         return member;
     }
 
-    @GetMapping("/test")
+    @GetMapping("/list")
+    public List<Member> list() {
+        List<Member> members = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> Member.builder()
+                        .email("user" + i + "@test.org")
+                        .password("12345678")
+                        .userName("사용자" + i)
+                        .regDt(LocalDateTime.now())
+                        .build())
+                .toList();
+
+        return members;
+    }
+
+    @GetMapping(path="/test", produces = "text/html;charset=UTF-8")
     public String test() {
         // Content-Type: text/plain
         return "안녕하세요!";
+    }
+
+    @GetMapping("/test2")
+    public void test2() {
+        log.info("test2....");
+
     }
 }
