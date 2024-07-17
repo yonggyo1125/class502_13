52b5c185e06bdca20b5aae3240ed9e516905f548
yonggyo1125 <yonggyo00@kakao.com>
2024-07-17 18:10:07 +0900

    .

--- 1. 웹표준 기술/보강/20240716/todo.js
+++ 1. 웹표준 기술/보강/20240716/todo.js
@@ -1,3 +1,23 @@
+const todos = {
+  // 스케줄 추가
+  add(todo) {
+    todo = todo ?? "";
+    todo = todo.trim();
+
+    if (!todo) {
+      throw new Error("스케줄을 입력하세요.");
+    }
+
+    const liEl = document.createElement("li");
+    liEl.append(todo);
+
+    const schedulesEl = document.getElementById("schedules");
+    schedulesEl.append(liEl);
+  },
+  // 스케줄 제거
+  remove() {},
+};
+
 window.addEventListener("DOMContentLoaded", function () {
   frmRegist.addEventListener("submit", function (e) {
     e.preventDefault();
