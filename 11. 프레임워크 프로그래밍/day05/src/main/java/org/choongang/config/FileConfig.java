ffd33c31b0b93f7c09aace2400f9fbdd42bf27db
yonggyo1125 <yonggyo00@kakao.com>
2024-07-17 09:42:28 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/FileConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/FileConfig.java
@@ -1,16 +1,19 @@
 package org.choongang.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class FileConfig implements WebMvcConfigurer {
+    @Value("${file.upload.path}")
+    private String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 파일 업로드 정적 경로 설정
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:///D:/uploads/");
+                .addResourceLocations("file:///" + uploadPath);
     }
 }
