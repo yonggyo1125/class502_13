df5c8d4b3d3eec4bb5b8f23e44f0f3c1ca1be4b4
yonggyo1125 <yonggyo00@kakao.com>
2024-07-19 14:19:03 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -112,7 +112,7 @@ public class MemberController {
 
     @GetMapping("/list")
     public String list2(Model model) {
-
+        /*
         Member member = Member.builder()
                 .email("user01@test.org")
                 .password("12345678")
@@ -121,6 +121,16 @@ public class MemberController {
                 .build();
 
         model.addAttribute("member", member);
+        */
+        List<Member> items = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> Member.builder()
+                        .email("user" + i + "@test.org")
+                        .userName("사용자" + i)
+                        .regDt(LocalDateTime.now())
+                        .build())
+                .toList();
+
+        model.addAttribute("items", items);
 
         return "member/list";
     }
