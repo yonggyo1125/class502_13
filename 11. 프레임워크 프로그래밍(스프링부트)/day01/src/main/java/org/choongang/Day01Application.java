5ad8239d3a135ae165b6472837ca7c1220d1c8f6
yonggyo1125 <yonggyo00@kakao.com>
2024-07-22 12:52:09 +0900

    수업 소스

---
+++ 11. 프레임워크 프로그래밍(스프링부트)/day01/src/main/java/org/choongang/Day01Application.java
@@ -0,0 +1,13 @@
+package org.choongang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day01Application.class, args);
+	}
+
+}
---
+++ 11. 프레임워크 프로그래밍(스프링부트)/day01/src/test/java/org/choongang/Day01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.choongang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
