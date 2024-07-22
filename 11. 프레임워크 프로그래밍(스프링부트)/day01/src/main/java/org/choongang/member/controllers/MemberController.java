4db63ba163d6de3cb1d2035e8aaca574c15576b5
yonggyo1125 <yonggyo00@kakao.com>
2024-07-22 15:01:21 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍(스프링부트)/day01/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day01/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,15 +1,17 @@
 package org.choongang.member.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/member")
 public class MemberController {
     @GetMapping("/join")
     public String join() {
-
+        log.info("코드 수정....");
         return "member/join";
     }
 }
