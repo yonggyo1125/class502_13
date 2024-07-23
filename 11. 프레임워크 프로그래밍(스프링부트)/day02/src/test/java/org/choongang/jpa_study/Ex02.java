076547799a19f7370499cd499bfe7a9458b667b9
yonggyo1125 <yonggyo00@kakao.com>
2024-07-23 12:32:48 +0900

    수업 소스

---
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex02.java
@@ -0,0 +1,42 @@
+package org.choongang.jpa_study;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.choongang.member.entities.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(properties = "spring.profiles.active=test")
+public class Ex02 {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @BeforeEach
+    void init() {
+        for (long i = 1L; i <= 10L; i++) {
+            Member member = new Member();
+            member.setSeq(i);
+            member.setEmail("user" + i + "@test.org");
+            member.setPassword("12345678");
+            member.setUserName("사용자" + i);
+            member.setCreatedAt(LocalDateTime.now());
+            em.persist(member); // 영속 상태
+        }
+
+        em.flush(); // DB 영구 반영
+    }
+
+    @Test
+    void test1() {
+
+    }
+}
