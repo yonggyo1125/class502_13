09ae95f6e01e92eb0beaa8e2a8425190cd52c6d2
yonggyo1125 <yonggyo00@kakao.com>
2024-07-23 12:03:55 +0900

    수업 소스

---
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/global/config/DBConfig.java
@@ -0,0 +1,10 @@
+package org.choongang.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class DBConfig {
+
+}
--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex01.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex01.java
@@ -3,28 +3,33 @@ package org.choongang.jpa_study;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceContext;
 import org.choongang.member.entities.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 @SpringBootTest
+@Transactional
 @TestPropertySource(properties = "spring.profiles.active=test")
 public class Ex01 {
 
-    @Autowired
-    private EntityManagerFactory emf;
+    //@Autowired
+    //private EntityManagerFactory emf;
+
+    //@Autowired
+    @PersistenceContext
+    private EntityManager em;
 
     @Test
     void test1() {
-        EntityManager em = emf.createEntityManager();
+        //EntityManager em = emf.createEntityManager();
 
-        EntityTransaction tx = em.getTransaction();
 
-        tx.begin(); // Transaction 시작
         Member member = new Member();
         member.setSeq(1L);
         member.setEmail("user01@test.org");
@@ -32,10 +37,24 @@ public class Ex01 {
         member.setUserName("사용자01");
         member.setCreatedAt(LocalDateTime.now());
 
-        em.persist(member);
+        em.persist(member); // 영속 상태 - 변화 감지 메모리에 있다, 변화 감지..
+
+        em.flush(); // INSERT 쿼리
+
+        em.detach(member); // 영속 상태 분리 - 변화 감지 X
+
+        member.setUserName("(수정)사용자01"); // 변경
+        member.setModifiedAt(LocalDateTime.now());
+
+        em.flush(); // UPDATE 쿼리
+
+        em.merge(member); // 분리된 영속 상태 -> 영속 상태 (변화 감지 상태)
 
         em.flush();
 
-        tx.commit();
+        //em.remove(member); // 제거 상태, 제거 X, 상태만..
+        //em.flush(); // DELETE 쿼리
+
+
     }
 }
