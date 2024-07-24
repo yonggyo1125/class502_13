742eb47d5df4bf3cc9768654c906e4853bac5cee
yonggyo1125 <yonggyo00@kakao.com>
2024-07-24 12:01:22 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex05.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex05.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 @SpringBootTest
 @TestPropertySource(properties = "spring.profiles.active=test")
 public class Ex05 {
@@ -18,7 +21,19 @@ public class Ex05 {
 
     @BeforeEach
     void init() {
+
+        List<Member> members = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> Member.builder()
+                        .email("user" + i + "@test.org")
+                        .password("12345678")
+                        .userName("사용자" + i)
+                        .authority(Authority.USER)
+                        .build()).toList();
+
+        memberRepository.saveAllAndFlush(members);
+        /*
         for (int i = 1; i <= 10; i++) {
+
             Member member = Member.builder()
                     .email("user" + i + "@test.org")
                     .password("12345678")
@@ -28,8 +43,9 @@ public class Ex05 {
 
             memberRepository.save(member);
         }
+        */
 
-        memberRepository.flush();
+        //memberRepository.flush();
 
         //member.setUserName("(수정)사용자01");
 
@@ -43,8 +59,20 @@ public class Ex05 {
         System.out.println(member);
 
         member.setUserName("(수정)사용자01");
+        memberRepository.save(member);
+
+        //memberRepository.flush();
 
         Member member2 = memberRepository.findById(1L).orElse(null);
         System.out.println(member2);
+
+        memberRepository.delete(member2);
+        memberRepository.flush();
+    }
+
+    @Test
+    void test2() {
+        List<Member> members = memberRepository.findAll();
+        members.forEach(System.out::println);
     }
 }
