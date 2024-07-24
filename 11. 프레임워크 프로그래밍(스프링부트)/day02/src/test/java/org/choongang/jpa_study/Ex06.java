01c0e47cd324e3a4d877afa195898b60fff07b08
yonggyo1125 <yonggyo00@kakao.com>
2024-07-24 12:30:15 +0900

    수업 소스

---
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex06.java
@@ -0,0 +1,41 @@
+package org.choongang.jpa_study;
+
+import org.choongang.member.constants.Authority;
+import org.choongang.member.entities.Member;
+import org.choongang.member.repositories.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@TestPropertySource(properties = "spring.profiles.active=test")
+public class Ex06 {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void init() {
+
+        List<Member> members = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> Member.builder()
+                        .email("user" + i + "@test.org")
+                        .password("12345678")
+                        .userName("사용자" + i)
+                        .authority(Authority.USER)
+                        .build()).toList();
+
+        memberRepository.saveAllAndFlush(members);
+    }
+
+    @Test
+    void test1() {
+        Member member = memberRepository.findByEmail("user2@test.org");
+        System.out.println(member);
+    }
+}
