6657eddd8646ada4438df517b0f10a55de10bcb5
yonggyo1125 <yonggyo00@kakao.com>
2024-07-24 14:45:50 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex06.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex06.java
@@ -63,5 +63,6 @@ public class Ex06 {
     @Test
     void test4() {
         List<Member> members = memberRepository.getMembers("%ser%", "%용%");
+        members.forEach(System.out::println);
     }
 }
