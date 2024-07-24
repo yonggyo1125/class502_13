42d39e1ef5a1d0e90807bf069ac2c7bb655a173a
yonggyo1125 <yonggyo00@kakao.com>
2024-07-24 09:35:49 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/global/entities/BaseEntity.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/global/entities/BaseEntity.java
@@ -3,17 +3,18 @@ package org.choongang.global.entities;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 @Getter @Setter
 @MappedSuperclass
 public abstract class BaseEntity {
-    @CreationTimestamp
+
+    @CreatedDate
     private LocalDateTime createdAt;
 
-    @UpdateTimestamp
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
 }
