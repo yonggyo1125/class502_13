684fb03b2e6c18b49d0985cf7c0e92fd3ba61c9d
yonggyo1125 <yonggyo00@kakao.com>
2024-07-24 11:47:56 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex05.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex05.java
@@ -1,7 +1,10 @@
 package org.choongang.jpa_study;
 
+import org.choongang.member.constants.Authority;
+import org.choongang.member.entities.Member;
 import org.choongang.member.repositories.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -15,6 +18,33 @@ public class Ex05 {
 
     @BeforeEach
     void init() {
+        for (int i = 1; i <= 10; i++) {
+            Member member = Member.builder()
+                    .email("user" + i + "@test.org")
+                    .password("12345678")
+                    .userName("사용자" + i)
+                    .authority(Authority.USER)
+                    .build();
 
+            memberRepository.save(member);
+        }
+
+        memberRepository.flush();
+
+        //member.setUserName("(수정)사용자01");
+
+        //memberRepository.save(member);
+        //memberRepository.flush();
+    }
+
+    @Test
+    void test1() {
+        Member member = memberRepository.findById(1L).orElse(null);
+        System.out.println(member);
+
+        member.setUserName("(수정)사용자01");
+
+        Member member2 = memberRepository.findById(1L).orElse(null);
+        System.out.println(member2);
     }
 }
