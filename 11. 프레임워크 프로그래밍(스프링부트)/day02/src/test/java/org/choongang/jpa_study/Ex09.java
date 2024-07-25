59d0785a3a640647ee4a75e2bbf7a7e0f0e7f552
yonggyo1125 <yonggyo00@kakao.com>
2024-07-25 10:20:45 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex09.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex09.java
@@ -1,11 +1,53 @@
 package org.choongang.jpa_study;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.choongang.board.entities.BoardData;
+import org.choongang.board.repositories.BoardDataRepository;
+import org.choongang.member.constants.Authority;
+import org.choongang.member.entities.Member;
+import org.choongang.member.repositories.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
 public class Ex09 {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private BoardDataRepository boardDataRepository;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @BeforeEach
+    void init() {
+        Member member = Member.builder()
+                .email("user01@test.org")
+                .password("12345678")
+                .userName("사용자01")
+                .authority(Authority.USER)
+                .build();
+
+        memberRepository.saveAndFlush(member);
+
+
+        List<BoardData> items = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> BoardData.builder()
+                        .subject("제목" + i)
+                        .content("내용" + i)
+                        .member(member)
+                        .build()).toList();
+
+    }
 }
