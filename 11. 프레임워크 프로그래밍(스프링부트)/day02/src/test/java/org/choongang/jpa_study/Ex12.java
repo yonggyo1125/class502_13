242da87b94db2d9f7db76f524975229d8b6ffd9c
yonggyo1125 <yonggyo00@kakao.com>
2024-07-26 10:24:31 +0900

    수업 소스

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex12.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex12.java
@@ -2,6 +2,9 @@ package org.choongang.jpa_study;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -136,11 +139,19 @@ public class Ex12 {
         andBuilder.and(orBuilder);
         */
 
+        PathBuilder<BoardData> pathBuilder = new PathBuilder<>(BoardData.class, "boardData");
+
+
         JPAQuery<BoardData> query = queryFactory.selectFrom(boardData)
                 .leftJoin(boardData.member)
                 .fetchJoin()
                 .where(andBuilder)
-                .limit(3); // 3개 레코드로 한정 - 갯수 제한
+                .offset(3) // 조회 시작 레코드 위치 3번 행부터 조회 시작
+                .limit(3) // 3개 레코드로 한정 - 갯수 제한
+                .orderBy(
+                        new OrderSpecifier(Order.DESC, pathBuilder.get("createdAt")),
+                        new OrderSpecifier(Order.ASC, pathBuilder.get("userName"))
+                );
                 //.where(boardData.seq.in(2L, 3L, 4L)); // BooleanExpression - Predicate
 
         List<BoardData> items = query.fetch();
