7dd6fe8d934ccaa37506e1e5f1cf27924aa38338
yonggyo1125 <yonggyo00@kakao.com>
2024-09-02 17:39:48 +0900

    .

---
+++ 15. 실무 프로젝트(p-5)/project/src/main/java/org/choongang/member/entities/Member.java
@@ -0,0 +1,14 @@
+package org.choongang.member.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long seq;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+    private String userName;
+}
