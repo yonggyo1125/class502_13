a71df16c557b0e3d787693a5b9d18413806202c2
yonggyo1125 <yonggyo00@kakao.com>
2024-09-04 10:40:24 +0900

    .

--- 16. 리액트 네이티브/exam01/src/app/counter/page.js
+++ 16. 리액트 네이티브/exam01/src/app/counter/page.js
@@ -9,16 +9,17 @@ const Counter = () => {
   const onIncrease = () => {
     setNum(num + 1);
     num2++;
-    console.log("num2", num2);
-  }
+    console.log('num2', num2);
+  };
 
+  const onDecrease = () => setNum(num - 1);
   return (
     <>
       <h1>{num}</h1>
-      <button type="button" onClick={() => setNum(num - 1)}>
+      <button type="button" onClick={onDecrease}>
         -1
       </button>
-      <button type="button" onClick={() => setNum(num + 1)}>
+      <button type="button" onClick={onIncrease}>
         +1
       </button>
     </>
