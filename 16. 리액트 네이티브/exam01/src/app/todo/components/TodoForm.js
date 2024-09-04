2fdc4bc56135c5e6a7b4419a8a7dec1bf1c315c2
yonggyo1125 <yonggyo00@kakao.com>
2024-09-04 11:14:25 +0900

    .

---
+++ 16. 리액트 네이티브/exam01/src/app/todo/components/TodoForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoForm = () => {
+  return <h1>Todo 양식</h1>;
+};
+
+export default TodoForm;
---
+++ 16. 리액트 네이티브/exam01/src/app/todo/components/TodoList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoList = () => {
+  return <h1>Todo 목록</h1>;
+};
+
+export default TodoList;
