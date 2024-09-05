314fcfa9ec323e19c72be1534b7ba67a9cc7c998
yonggyo1125 <yonggyo00@kakao.com>
2024-09-05 11:43:40 +0900

    .

--- 16. 리액트 네이티브/exam01/src/app/todo/containers/TodoContainer.js
+++ 16. 리액트 네이티브/exam01/src/app/todo/containers/TodoContainer.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { produce } from 'immer';
 import TodoForm from '../components/TodoForm';
 import TodoList from '../components/TodoList';
@@ -11,7 +11,7 @@ const TodoContainer = () => {
 
   const titleRef = useRef();
 
-  const onToggle = (id) => {
+  const onToggle = useCallback((id) => {
     /*
     const newItems = items.map((item) =>
       id === item.id ? { ...item, done: !item.done } : item,
@@ -34,14 +34,14 @@ const TodoContainer = () => {
           });
       }),
     );
-  };
+  }, []);
 
-  const onRemove = (id) => {
+  const onRemove = useCallback((id) => {
     // filter 메서드
     setItems((items) => items.filter((item) => item.id !== id));
-  };
+  }, []);
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     /**
      * e.target
      * //e.currentTarget
@@ -50,46 +50,50 @@ const TodoContainer = () => {
     const name = e.target.name;
     const value = e.target.value;
     setForm((form) => ({ ...form, [name]: value }));
-  };
+    //setForm({ ...form, [name]: value });
+  }, []);
 
-  const onClick = (done) => {
+  const onClick = useCallback((done) => {
     setForm((form) => ({ ...form, done }));
-  };
+  }, []);
 
-  const onSubmit = (e) => {
-    e.preventDefault(); // 양식 기본 동작 차단
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault(); // 양식 기본 동작 차단
 
-    // 검증
-    const _errors = {};
-    let hasErrors = false;
+      // 검증
+      const _errors = {};
+      let hasErrors = false;
 
-    const requiredFields = {
-      title: '제목을 입력하세요.',
-      content: '내용을 입력하세요.',
-    };
+      const requiredFields = {
+        title: '제목을 입력하세요.',
+        content: '내용을 입력하세요.',
+      };
 
-    for (const [field, message] of Object.entries(requiredFields)) {
-      if (!form[field] || !form[field].trim()) {
-        _errors[field] = _errors[field] ?? [];
-        _errors[field].push(message);
-        hasErrors = true;
+      for (const [field, message] of Object.entries(requiredFields)) {
+        if (!form[field] || !form[field].trim()) {
+          _errors[field] = _errors[field] ?? [];
+          _errors[field].push(message);
+          hasErrors = true;
+        }
       }
-    }
 
-    setErrors(_errors);
-    if (hasErrors) {
-      // 검증 실패
-      return;
-    }
+      setErrors(_errors);
+      if (hasErrors) {
+        // 검증 실패
+        return;
+      }
 
-    // 일정 등록 처리
-    const nextId = items ? Math.max(...items.map((item) => item.id)) + 1 : 1;
-    setItems((items) => items.concat({ ...form, id: nextId }));
+      // 일정 등록 처리
+      const nextId = items ? Math.max(...items.map((item) => item.id)) + 1 : 1;
+      setItems((items) => items.concat({ ...form, id: nextId }));
 
-    // 양식 초기화
-    setForm({});
-    titleRef.current.focus();
-  };
+      // 양식 초기화
+      setForm({});
+      titleRef.current.focus();
+    },
+    [form, items],
+  );
 
   return (
     <>
