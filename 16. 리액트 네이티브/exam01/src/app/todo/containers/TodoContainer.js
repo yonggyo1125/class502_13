e9130e4cf208aabaf80b85ec989ca6bd4c2db156
yonggyo1125 <yonggyo00@kakao.com>
2024-09-05 10:01:24 +0900

    .

--- 16. 리액트 네이티브/exam01/src/app/todo/containers/TodoContainer.js
+++ 16. 리액트 네이티브/exam01/src/app/todo/containers/TodoContainer.js
@@ -6,9 +6,9 @@ import TodoList from '../components/TodoList';
 
 const TodoContainer = () => {
   const [items, setItems] = useState([
-    { id: 1, title: '할일1', done: true },
-    { id: 2, title: '할일2', done: false },
-    { id: 3, title: '할일3', done: false },
+    { id: 1, title: '할일1', content: '내용1', done: true },
+    { id: 2, title: '할일2', content: '내용2', done: false },
+    { id: 3, title: '할일3', content: '내용3', done: false },
   ]);
   const [form, setForm] = useState({});
 
