00e03669857b4a958d1e6f3f8eaead9afae118d9
yonggyo1125 <yonggyo00@kakao.com>
2024-09-05 12:51:59 +0900

    .

--- 16. 리액트 네이티브/exam01/src/app/counter2/Counter.js
+++ 16. 리액트 네이티브/exam01/src/app/counter2/Counter.js
@@ -1,5 +1,12 @@
+'use client';
+import React, { useReducer } from 'react';
+
 const Counter = () => {
-  return <></>;
+  return (
+    <>
+      <h1>0</h1>
+    </>
+  );
 };
 
 export default Counter;
