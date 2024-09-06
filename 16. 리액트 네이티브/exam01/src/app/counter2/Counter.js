328026a64addb3da7a58c40a36b74cb9ea203bf4
yonggyo1125 <yonggyo00@kakao.com>
2024-09-06 09:55:31 +0900

    .

--- 16. 리액트 네이티브/exam01/src/app/counter2/Counter.js
+++ 16. 리액트 네이티브/exam01/src/app/counter2/Counter.js
@@ -1,10 +1,29 @@
 'use client';
 import React, { useReducer } from 'react';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREASE':
+      return { number: state.number + 1 };
+    case 'DECREASE':
+      return { number: state.number - 1 };
+    default:
+      return state;
+  }
+}
+
 const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { number: 0 });
+  const { number } = state;
   return (
     <>
-      <h1>0</h1>
+      <h1>{number}</h1>
+      <button type="button" onClick={() => dispatch({ type: 'DECREASE' })}>
+        -1
+      </button>
+      <button type="button" onClick={() => dispatch({ type: 'INCREASE' })}>
+        +1
+      </button>
     </>
   );
 };
