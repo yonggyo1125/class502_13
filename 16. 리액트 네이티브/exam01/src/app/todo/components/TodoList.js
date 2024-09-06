a018ee1e4fa821a8f2db82408bec30d89dfb615a
yonggyo1125 <yonggyo00@kakao.com>
2024-09-06 11:45:40 +0900

    .

--- 16. 리액트 네이티브/exam01/src/app/todo/components/TodoList.js
+++ 16. 리액트 네이티브/exam01/src/app/todo/components/TodoList.js
@@ -3,27 +3,41 @@ import { FaCheckSquare, FaRegCheckSquare } from 'react-icons/fa';
 import { CiSquareRemove } from 'react-icons/ci';
 import styled from 'styled-components';
 
-const ItemBox = ({item, onToggle, onRemove}) => {
-  const {id, title, content, done } = item;
+const ItemBox = ({ item, onToggle, onRemove }) => {
+  const { id, title, content, done } = item;
   return (
     <li key={id} onClick={() => onToggle(id)}>
-                <div>
-                  {done ? <FaCheckSquare /> : <FaRegCheckSquare />}
-                  {title}
-                  <CiSquareRemove onClick={() => onRemove(id)} />
-                </div>
-                {content && <div>{content}</div>}
-      </li>);
+      <div>
+        {done ? <FaCheckSquare /> : <FaRegCheckSquare />}
+        {title}
+        <CiSquareRemove onClick={() => onRemove(id)} />
+      </div>
+      {content && <div>{content}</div>}
+    </li>
+  );
 };
 
+const StyledItemBox = styled(ItemBox)`
+  display: flex;
+  border: 1px solid #000;
+  padding: 10px;
+  & + & {
+    margin-top: 10px;
+  }
+`;
 
 const TodoList = ({ items, onToggle, onRemove }) => {
   return (
     <ul>
       {items &&
         items.length > 0 &&
-        items.map(({ id, title, content, done }) => (
-          
+        items.map((item) => (
+          <ItemBox
+            key={item.id}
+            item={item}
+            onToggle={onToggle}
+            onRemove={onRemove}
+          />
         ))}
     </ul>
   );
