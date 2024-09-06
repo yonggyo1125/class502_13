da895825fb3abe9065feb00ce2a62fc0652cbc5c
yonggyo1125 <yonggyo00@kakao.com>
2024-09-06 14:10:59 +0900

    .

--- 16. 리액트 네이티브/exam01/src/app/context/ContextEx2.js
+++ 16. 리액트 네이티브/exam01/src/app/context/ContextEx2.js
@@ -8,9 +8,26 @@ const ContextEx2 = () => {
     actions: { setIsLogin, setUserInfo },
   } = useContext(UserContext);
 
+  const onLogin = () => {
+    setIsLogin(true);
+    setUserInfo({ userName: '이이름', email: 'user01@test.org' });
+  };
+
+  const onLogout = () => {
+    setIsLogin(false);
+    setUserInfo(null);
+  };
   return (
     <div>
-      <button type="button"></button>
+      {isLogin ? (
+        <button type="button" onClick={onLogout}>
+          로그아웃
+        </button>
+      ) : (
+        <button type="button" onClick={onLogin}>
+          로그인
+        </button>
+      )}
     </div>
   );
 };
